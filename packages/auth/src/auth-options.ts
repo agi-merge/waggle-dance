106497ff1c23a496ae5c7d90f125f788acac939a
jon <jondwillis@gmail.com>
2023-04-27 14:27:05 -0700

    github and creds auth too

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,6 +1,8 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type DefaultSession, type NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
 
 import { prisma } from "@acme/db";
 
@@ -46,14 +48,63 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.DISCORD_ID as string,
       clientSecret: process.env.DISCORD_SECRET as string,
     }),
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Discord provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     **/
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "What's the code?",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        // username: {
+        //   label: "Username",
+        //   type: "text",
+        //   placeholder: "Ask Jon",
+        // },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Ask Jon",
+        },
+      },
+      authorize(credentials) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        const pass = credentials?.password;
+        switch (pass) {
+          case "jonwillis":
+            return {
+              id: "0",
+              name: "Jon Willis",
+            };
+          case "zachstein":
+          case "jordanmack":
+          case "jacksonolsen":
+          case "amywillis":
+          case "davewillis":
+          case "carolwillis":
+          case "marissajimenez":
+            return {
+              id: "1",
+              name: "Close",
+            };
+          case "friendsandfamily":
+            return {
+              id: "2",
+              name: "Friends and Family",
+            };
+        }
+
+        return null;
+      },
+    }),
   ],
 };
