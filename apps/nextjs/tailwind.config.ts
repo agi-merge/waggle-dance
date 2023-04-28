c512b66a4e866ad0f6de0750ac5b674689439e66
jon <jondwillis@gmail.com>
2023-04-27 17:20:10 -0700

    Revert "tailwind elements ui lib"
    This reverts commit 9068c47481e47a2214a338946542940569680ce5.
    
    # Conflicts:
    #	apps/expo/tailwind.config.js
    #	apps/nextjs/src/pages/_app.tsx

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -3,11 +3,6 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@acme/tailwind-config";
 
 export default {
-  content: ["./src/**/*.tsx", "./node_modules/tw-elements/dist/js/**/*.js"],
+  content: ["./src/**/*.tsx"],
   presets: [baseConfig],
-  darkMode: "class",
-  plugins: [require("tw-elements/dist/plugin.cjs")],
-  theme: {
-    extend: {},
-  },
 } satisfies Config;
