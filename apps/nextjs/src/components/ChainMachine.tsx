9dd7af996d0eab883c3b70470d082f23c44ae09c
jon <jondwillis@gmail.com>
2023-04-28 16:19:56 -0700

    v1 chainmachine...

--- apps/nextjs/src/components/ChainMachine.tsx
+++ apps/nextjs/src/components/ChainMachine.tsx
@@ -1,68 +1,91 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useMachine } from "react-robot";
-import { createMachine, invoke, state, transition } from "robot3";
+import {
+  createMachine,
+  guard,
+  immediate,
+  invoke,
+  reduce,
+  state,
+  transition,
+} from "robot3";
 
-// State machine definition
-const taskMachine = createMachine({
-  idle: state(transition("start", "working")),
-  working: invoke(
-    (ctx) => ctx.onCompletion,
-    transition("done", "completed", (ctx) => {
-      ctx.review();
-    }),
-  ),
-  completed: state(),
-});
+type TaskStatus = "idle" | "working" | "completed";
 
-const ChainMachine = () => {
-  // Define the state
-  const [tasks, setTasks] = React.useState([]);
+interface TaskData {
+  id: number;
+  status: TaskStatus;
+  onCompletion: () => void;
+}
 
-  // Handle plan completion (progressing to review)
+const isTaskWorking = (task: TaskData) => task.status === "working";
+
+const taskMachine = (onCompletion: () => void) =>
+  createMachine({
+    idle: state(transition("start", "working")),
+    working: state(
+      immediate("completed", guard(isTaskWorking)),
+      transition("done", "completed"),
+    ),
+    completed: invoke(onCompletion, transition("done", "completed")),
+  });
+
+const Task: React.FC<TaskData> = ({ id, status, onCompletion }) => {
+  const machine = taskMachine(onCompletion);
+  const [current] = useMachine(machine);
+
+  useEffect(() => {
+    if (status === "working") {
+      machine.send("start");
+    }
+  }, [status, machine]);
+
+  return (
+    <div>
+      Task {id}: {current.name}
+    </div>
+  );
+};
+
+const ChainMachine: React.FC = () => {
+  const [tasks, setTasks] = useState<TaskData[]>([]);
+
+  const addNewTask = () => {
+    setTasks((prevTasks): TaskData[] => [
+      ...prevTasks,
+      {
+        id: prevTasks.length,
+        status: "idle",
+        onCompletion: () => console.log(`Task ${prevTasks.length} completed`),
+      },
+    ]);
+  };
+
   const onPlanCompletion = useCallback(
-    (id) => {
+    (id: number) => {
       setTasks(
         tasks.map((task) =>
-          task.id === id ? { ...task, status: "review" } : task,
+          task.id === id ? { ...task, status: "completed" } : task,
         ),
       );
     },
     [tasks],
   );
 
-  // Add tasks to the tasks array
-  const addTask = () => {
-    const nextId = tasks.length + 1;
-    setTasks([...tasks, { id: nextId, status: "working" }]);
-  };
-
   return (
     <div>
       <h1>Chain Machine</h1>
-      <button onClick={addTask}>Add Task</button>
+      <button onClick={addNewTask}>Add Task</button>
       <div>
         {tasks.map((task) => (
           <Task
             key={task.id}
             id={task.id}
+            status={task.status}
             onCompletion={() => onPlanCompletion(task.id)}
           />
         ))}
       </div>
-    </div>
-  );
-};
-
-const Task = ({ id, onCompletion }) => {
-  const machineContext = React.useMemo(
-    () => ({ onCompletion: () => onCompletion() }),
-    [onCompletion],
-  );
-  const [current] = useMachine(taskMachine, machineContext);
-
-  return (
-    <div>
-      Task {id}: {current.name}
     </div>
   );
 };
--- apps/nextjs/src/components/GoalWorkspace.tsx
+++ apps/nextjs/src/components/GoalWorkspace.tsx
@@ -1,6 +1,7 @@
 import { useMachine } from "react-robot";
 
 import { api, type RouterOutputs } from "~/utils/api";
+import ChainMachine from "./ChainMachine";
 
 interface GoalWorkspaceProps {
   goal: RouterOutputs["goal"]["all"][number];
@@ -10,18 +11,7 @@ interface GoalWorkspaceProps {
 const GoalWorkspace = ({ goal, onDelete }: GoalWorkspaceProps) => {
   return (
     <div className="flex flex-row rounded-lg bg-white/10 p-4 transition-all hover:scale-[101%]">
-      <div className="flex-grow">
-        <h2 className="text-2xl font-bold text-pink-400">{goal.title}</h2>
-        <p className="mt-2 text-sm">{goal.content}</p>
-      </div>
-      <div>
-        <span
-          className="cursor-pointer text-sm font-bold uppercase text-pink-400"
-          onClick={onDelete}
-        >
-          Delete
-        </span>
-      </div>
+      <ChainMachine />
     </div>
   );
 };
