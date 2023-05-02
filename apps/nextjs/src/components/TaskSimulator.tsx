46ad4beef3676a31ac40d4bcc68b4437fcbe39d4
Jon Willis <jondwillis@gmail.com>
2023-05-01 22:44:05 -0700

    super close, no cancellation

--- apps/nextjs/src/components/TaskSimulator.tsx
+++ apps/nextjs/src/components/TaskSimulator.tsx
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 import { Button, Stack, Typography } from "@mui/joy";
 import Balamb, { BalambError, SeedDef } from "balamb";
 
+type PlanResult = {
+  planId: string;
+  tasks: string[];
+};
+
 type TaskResult = {
-  name: string;
-  result: any;
+  taskId: string;
+  result: string;
+};
+
+type Review = {
+  overall: number;
 };
 
 type ReviewResult = {
   target: string;
-  success: boolean;
+  review: Review;
 };
 
 // Usage example:
@@ -19,82 +28,155 @@ class TaskSimulation {
     taskName: string,
     onReviewFailure: (target: string, error: Error) => void,
   ) {
-    const planTask = () => {
-      const id = `plan-${taskName}-${this.generateTaskName()}`;
-      const planTask: SeedDef<string[], void> = {
-        id,
-        description: "Plan a tasks to achieve goal",
-        plant: async () => {
-          const subTaskCount = 1 + Math.floor(Math.random() * 10);
-          var tasks: string[] = [];
-          for (let i = 0; i < subTaskCount; i++) {
-            const newTaskId = `subTask-${this.generateTaskName()}`;
-            tasks.push(newTaskId);
-          }
-          console.log(`Planned ${subTaskCount} new tasks for ${id}`);
-          return tasks;
-        },
-      };
-      return planTask;
+    // const planTask = (taskName: string) => {
+    // let id = `plan-${taskName}-${this.generateTaskName()}`;
+    // const planTask: SeedDef<string[], void> = {
+    //   id,
+    //   description: "Plan a tasks to achieve goal",
+    //   plant: async () => {
+    //     const subTaskCount = 1 + Math.floor(Math.random() * 10);
+    //     var tasks: string[] = [];
+    //     for (let i = 0; i < subTaskCount; i++) {
+    //       const newTaskId = `subTask-${this.generateTaskName()}`;
+    //       tasks.push(newTaskId);
+    //     }
+    //     console.log(`Planned ${subTaskCount} new tasks for ${id}`);
+    //     return tasks;
+    //   },
+    // };
+    //   return planTask;
+    // };
+
+    // const executeTask = (taskName: string) => {
+    // id = `execute-${taskName}-${this.generateTaskName()}`;
+    // const executeTask: SeedDef<TaskResult, void> = {
+    //   id,
+    //   description: "Execute a task and return its result",
+    //   plant: async () => {
+    //     // Simulate task execution
+    //     const result = Math.random();
+    //     console.log(`Executed task ${id} with result ${result}`);
+    //     return { name: id, result } as TaskResult;
+    //   },
+    //   dependsOn: { name: planTask() },
+    // };
+    //   return executeTask;
+    // };
+
+    // const reviewTask = (taskName: string) => {
+    // const reviewTask: SeedDef<ReviewResult, { e: TaskResult }> = {
+    //   id: `review-${taskName}-${this.generateTaskName()}`,
+    //   description: "Review a task and determine if it's successful",
+    //   dependsOn: { e: executeTask },
+    //   plant: async ({ e }) => {
+    //     // Simulate review with a 15% chance of failure
+    //     const success = Math.random() > 0.15;
+    //     console.log(`Reviewed task ${e.name} with result ${success}`);
+    //     return { target: e.name, success };
+    //   },
+    // };
+    //   return reviewTask;
+    // };
+    const subTaskCount = 1 + Math.floor(Math.random() * 10);
+    var tasks: string[] = [];
+    for (let i = 0; i < subTaskCount; i++) {
+      const newTaskId = `subTask-${this.generateTaskName()}`;
+      tasks.push(newTaskId);
+    }
+    console.log(`Planned ${subTaskCount} new tasks for ${taskName}`);
+
+    const plan: SeedDef<PlanResult, void> = {
+      id: `plan-${taskName}`,
+      description: "Plan tasks to achieve goal",
+      plant: async () => {
+        return { planId: `plan-${taskName}`, tasks };
+      },
+    };
+    // const getSubtaskResults: SeedDef<TaskResult[], { planResult: PlanResult }> =
+    //   {
+    //     id: `getSubtaskResults-${this.generateTaskName()}-${plan.id}`,
+    //     description: "Get results of subtasks",
+    //     dependsOn: { planResult: plan },
+    //     plant: async ({ planResult }) => {
+    //       const all = planResult.tasks.map(
+    //         (task) =>
+    //           ({ taskId: task, result: `${Math.random()}` } as TaskResult),
+    //       );
+    //       const ret = await Promise.all(all);
+    //       return ret;
+    //     },
+    //   };
+
+    const getSubtaskResult = (of: string) => {
+      const getSubtaskResult: SeedDef<TaskResult, { planResult: PlanResult }> =
+        {
+          id: `getSubtaskResult-${of}`,
+          description: "Get results of subtasks",
+          dependsOn: { planResult: plan },
+          plant: async ({ planResult }) => {
+            const result = `${Math.random()}`;
+            console.log(`Got result ${result} for ${of}`);
+            return { taskId: of, result } as TaskResult;
+          },
+        };
+      return getSubtaskResult;
     };
 
-    const executeTask = () => {
-      const id = `execute-${taskName}-${this.generateTaskName()}`;
-      const executeTask: SeedDef<TaskResult, void> = {
-        id,
-        description: "Execute a task and return its result",
-        plant: async () => {
-          // Simulate task execution
-          const result = Math.random();
-          console.log(`Executed task ${id} with result ${result}`);
-          return { name: id, result } as TaskResult;
-        },
-        dependsOn: { name: planTask() },
-      };
-      return executeTask;
-    };
-
-    const reviewTask = () => {
-      const reviewTask: SeedDef<ReviewResult, { e: TaskResult }> = {
-        id: `review-${taskName}-${this.generateTaskName()}`,
-        description: "Review a task and determine if it's successful",
-        dependsOn: { e: executeTask() },
-        plant: async ({ e }) => {
-          // Simulate review with a 15% chance of failure
-          const success = Math.random() > 0.15;
-          console.log(`Reviewed task ${e.name} with result ${success}`);
-          return { target: e.name, success };
+    const reviewSubtask = (of: string) => {
+      const reviewSubtask: SeedDef<ReviewResult, { taskResult: TaskResult }> = {
+        id: `review-${of}`,
+        description: "Review a plan and determine if needs cancellation",
+        dependsOn: { taskResult: getSubtaskResult(of) },
+        plant: async ({ taskResult }) => {
+          const review: Review = {
+            overall: Math.random(),
+          };
+          console.log(
+            `Reviewed plan ${of} ${taskResult} with result ${review.overall}`,
+          );
+          return { target: plan.id, review };
         },
       };
-      return reviewTask;
+      return reviewSubtask;
     };
-
-    const taskResult = await Balamb.run([
-      planTask(),
-      // { ...planTask(), args: {} },
-      // { ...executeTask(), args: { name: taskName } },
-      // { ...reviewTask(), args: { target: taskName } },
-    ]);
+    const reviewPlan: SeedDef<ReviewResult, { planResult: PlanResult }> = {
+      id: `review-${plan.id}`,
+      description: "Review a plan and determine if needs cancellation",
+      dependsOn: { planResult: plan },
+      plant: async ({ planResult }) => {
+        const review: Review = {
+          overall: Math.random(),
+        };
+        console.log(`Reviewed plan ${plan.id} with result ${review.overall}`);
+        return { target: plan.id, review };
+      },
+    };
+    const seeds = tasks.map((task) => {
+      return reviewSubtask(task);
+    });
+    const taskResult = await Balamb.run(seeds);
     if (taskResult instanceof BalambError) {
       console.error(taskResult);
       onReviewFailure(taskName, taskResult);
     } else {
-      const reviewResult = await Balamb.run([
-        { ...reviewTask(), args: { target: taskResult } },
-      ]);
-
-      if (reviewResult instanceof BalambError) {
-        // const errMessage = `Task review failed for task ${taskName}`;
-        console.error(reviewResult);
-        onReviewFailure(taskName, reviewResult);
-      } else {
-        console.log(
-          `Task ${taskName} executed successfully with result ${JSON.stringify(
-            taskResult,
-          )}`,
-        );
-      }
+      console.log(JSON.stringify(taskResult));
     }
+    //   const reviewResult = await Balamb.run([
+    //     { ...reviewTask(), args: { target: taskResult } },
+    //   ]);
+
+    //   if (reviewResult instanceof BalambError) {
+    //     // const errMessage = `Task review failed for task ${taskName}`;
+    //     console.error(reviewResult);
+    //     onReviewFailure(taskName, reviewResult);
+    //   } else {
+    //     console.log(
+    //       `Task ${taskName} executed successfully with result ${JSON.stringify(
+    //         taskResult,
+    //       )}`,
+    //     );
+    //   }
+    // }
   }
 
   generateTaskName(): string {
@@ -137,11 +219,6 @@ const TaskSimulator = () => {
     <Stack>
       <Button onClick={handleRunSimulation}>Run Simulation</Button>
       <Typography>{JSON.stringify(simulation)}</Typography>
-      {/* <ul>
-        {log.map((entry, index) => (
-          <li key={index}>{entry}</li>
-        ))}
-      </ul> */}
     </Stack>
   );
 };
