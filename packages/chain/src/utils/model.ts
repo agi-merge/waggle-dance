5cf0a8913a42cc30528a18a7fc6a360174c2e362
Jon Willis <jondwillis@gmail.com>
2023-05-07 13:34:54 -1000

    for now, have ModelCreationProps extends OpenAIInput

--- packages/chain/src/utils/model.ts
+++ packages/chain/src/utils/model.ts
@@ -5,17 +5,9 @@ import { LLM, type ModelCreationProps } from "./types";
 
 export const createModel = (creationProps: ModelCreationProps): OpenAI => {
   console.log(`createModel: ${JSON.stringify(creationProps)}`);
-  const { temperature, maxTokens, modelName, verbose, streaming, callbacks } =
-    creationProps;
   return new OpenAI(
     {
-      temperature,
-      modelName,
-      maxTokens,
-      verbose,
-      streaming,
-      callbacks,
-      maxConcurrency: 8,
+      ...creationProps,
     },
     {
       basePath: "https://oai.hconeai.com/v1", // TODO: move this to .env
--- packages/chain/src/utils/types.ts
+++ packages/chain/src/utils/types.ts
@@ -1,4 +1,6 @@
-import { type CallbackManager } from "langchain/callbacks";
+import { CallbackManager } from "langchain/callbacks";
+import { BaseLLMParams } from "langchain/llms/base";
+import { OpenAIInput } from "langchain/llms/openai";
 import { z } from "zod";
 
 const GPT_35_TURBO = "gpt-3.5-turbo";
@@ -8,18 +10,17 @@ export enum LLM {
   gpt4 = GPT_4,
 }
 
-export type ModelCreationProps = {
-  // customApiKey: string;
-  customModelName?: string;
-  // customTemperature: number;
-  // customMaxLoops: number;
-  modelName: string;
-  temperature?: number;
-  maxTokens?: number;
-  streaming?: boolean;
-  callbacks?: CallbackManager;
-  verbose?: boolean;
+type OpenAIKeyProvider = {
+  openAIApiKey?: string;
 };
+export interface ModelCreationProps
+  extends Partial<OpenAIInput>,
+    BaseLLMParams,
+    OpenAIKeyProvider {
+  verbose?: boolean;
+  callbacks?: CallbackManager;
+  modelName: LLM;
+}
 
 export const packetParser = z.object({
   type: z.enum([
