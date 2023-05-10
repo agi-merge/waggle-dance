891b588dd0dbc5e0b2dcbcf31e8dfdedd9e1834b
Chaz <chaz.mateer@gmail.com>
2023-05-09 20:22:05 -0400

    Add prebuild script and associated tools folder with a script that verifies that expected env vars have been added to env.mjs schema validation.

---
+++ prebuild.mjs
@@ -0,0 +1,23 @@
+import { checkEnv } from "./tools/check-env.mjs";
+
+// Fun message colors
+const RESET = '\x1b[0m';
+const RED = '\x1b[31m';
+const GREEN = '\x1b[32m';
+const YELLOW = '\x1b[33m';
+const BLUE = '\x1b[34m';
+
+console.log(BLUE + "🏃‍♀️ Running prebuild script..." + RESET);
+
+console.log("🕵️‍♀️ Checking envs...");
+try {
+  checkEnv();
+  console.log(GREEN + "✅ Envs look all good!" + RESET);
+} catch (error) {
+  console.error(RED + "❌ Error:" + RESET, error);
+  process.exit(1);
+}
+
+// Add more prebuild scripts here...
+
+console.log(BLUE + "🏁 Done!" + RESET);
