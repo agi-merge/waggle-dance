b5c40ad5800b4d364ecd53229e024a0c2504d9f2
Jon Willis <jondwillis@gmail.com>
2023-05-17 20:37:45 -0700

    stream plan

--- apps/nextjs/src/pages/api/chain/plan.ts
+++ apps/nextjs/src/pages/api/chain/plan.ts
@@ -1,8 +1,6 @@
-import { type NextRequest } from "next/server";
-
+import { NextRequest } from "next/server";
 import { planChain } from "@acme/chain";
-
-import { type StrategyRequestBody } from "./types";
+import { StrategyRequestBody } from "./types";
 
 export const config = {
   runtime: "edge",
@@ -11,8 +9,33 @@ export const config = {
 const handler = async (req: NextRequest) => {
   const { creationProps, goal } = (await req.json()) as StrategyRequestBody;
   const planResult = await planChain(creationProps, goal);
-  const json = JSON.stringify(planResult);
-  return new Response(json, {
+
+  const encoder = new TextEncoder();
+  const readableStream = new ReadableStream({
+    start(controller) {
+      const json = JSON.stringify(planResult);
+      const chunks = json.match(/.{1,1024}/g); // Split the JSON string into chunks of 1024 characters
+
+      if (chunks) {
+        for (const chunk of chunks) {
+          controller.enqueue(encoder.encode(chunk));
+        }
+      }
+
+      controller.close();
+    },
+  });
+
+  // const transformStream = new TransformStream({
+  //   transform(chunk, controller) {
+  //     encoder
+  //     const data = encoder.decode(chunk);
+  //     // Process the data as needed
+  //     controller.enqueue(encoder.encode(data));
+  //   },
+  // });
+
+  return new Response(readableStream/*.pipeThrough(transformStream)*/, {
     status: 200,
     headers: {
       "content-type": "application/json",
