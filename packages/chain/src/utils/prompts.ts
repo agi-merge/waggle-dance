e073354e843e833bf5f0a0af728d12bb0be465f0
jon <jondwillis@gmail.com>
2023-05-18 01:39:45 -0700

    fix prompts

--- packages/chain/src/utils/prompts.ts
+++ packages/chain/src/utils/prompts.ts
@@ -60,11 +60,8 @@ ${goal}
 <ReturnSchema>
 ${returnType(llmName)}
 </ReturnSchema>
-<Constitution>
-Ensure the DAG maximizes the qualities by which it is judged: [Coherence, Creativity, Efficiency, Directness, Resourcefulness, Accuracy, Ethics]
-</Constitution>
+Return the first level of a DAG in <ReturnSchema> that represents an expertly planned, embarassingly concurrent (consider dependencies!) solver of<UserGoal> for <UserGoal>'s PDDL Domain and Problem.
 `.trim(),
-Return the first level of a DAG in <ReturnSchema>that represents an expertly planned, embarassingly concurrent (consider dependencies!) solver of<UserGoal> for <UserGoal>'s PDDL Domain and Problem.
     ],
 
     execute: [
