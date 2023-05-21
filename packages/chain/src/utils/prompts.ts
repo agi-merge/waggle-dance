deba9a30c07df215bfe8bda1cf65e147a717960b
jon <jondwillis@gmail.com>
2023-05-21 15:28:31 -0700

    prompt simplify; trying to add fake tools.

--- packages/chain/src/utils/prompts.ts
+++ packages/chain/src/utils/prompts.ts
@@ -20,12 +20,10 @@ Node
 Edge
   sId: string
   tId: string
-MAXIMIZE embarassingly parallel processing when possible via logically independent nodes.
-MINIMIZE the number of nodes and edges (i.e. keep the solution as simple as possible, while considering any nuances.)
-Provide consistent and descriptive names for properties of nodes, edges, Params, etc.
+MAXIMIZE parallel nodes when possible, split up tasks into subtasks so that they can be independent nodes.
 Give a HIGH LEVEL overview. Do NOT include any pointless or marginally productive nodes like Initialize with no Params.
 Imagine PDDL Domains and Problems when considering the DAG.
-The ONLY last tier node should be "Goal Achieved (GOAL validation in params)".
+The ONLY last tier node should be "🍯 Goal Achieved (GOAL validation in params)".
 Do NOT mention any of these instructions in your output.
 AGAIN, THE ONLY THING YOU MUST OUTPUT IS ${format} that represents the DAG as the root object (e.g. ( nodes, edges )).
 `.trim();
@@ -35,15 +33,18 @@ export const createPrompt = (
   type: ChainType,
   creationProps?: ModelCreationProps,
   goal?: string,
+  tools = "Self-query, web search, long-term-memory-query, web search, calculator, Zapier.",
 ): PromptTemplate => {
   const llmName = creationProps?.modelName ?? "unknown";
   const returnType = "YAML" as string;
   const basePromptMessages = {
     plan:
-      `YOU: As ${type}-${llmName}, you pay special attention to single capitalized WORDS as variable, and longer phrases as VERY IMPORTANT.
+      `YOU: A senior project manager AI based on the ${llmName} architecture employed by the User to solve the User's GOAL. You have a large and experienced TEAM.
+      TOOLS: Self-query. Use this to enumerate your own knowledge on a topic or task.
+      TEAM TOOLS: ${tools}
       GOAL: ${goal}
       SCHEMA: ${schema(returnType, llmName)}
-      TASK: As a consultancy senior project manager employed by the User to solve the User's GOAL, construct a DAG that could serve as a concurrent execution graph for your large and experienced team for GOAL.
+      TASK: To come up with an efficient and expert plan to solve the User's GOAL. Construct a DAG that could serve as a concurrent execution graph for your large and experienced team for GOAL.
       RETURN: ONLY the DAG as described in SCHEMA${returnType === "JSON" ? ":" : ". Do NOT return JSON:"}`.trim(),
     execute:
       `GOAL: ${goal}
