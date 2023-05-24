2e9e0cc4e60baa7d1eb31b4bfb3973830b381581
jon <jondwillis@gmail.com>
2023-05-24 09:12:08 -0700

    bump visible version

--- apps/nextjs/src/constants.ts
+++ apps/nextjs/src/constants.ts
@@ -1,7 +1,7 @@
 export const app = {
   name: "🐝💃.ai",
   description: "Automate complex tasks with swarms of LLMs",
-  version: process.env.NEXT_PUBLIC_APP_VERSION ?? "04.20.23-alpha",
+  version: process.env.NEXT_PUBLIC_APP_VERSION ?? "05.24.23-alpha",
   localStorageKeys: {
     goal: "AGI:MERGE:GOAL:STORE",
     preferences: "AGI:MERGE:PREFERENCES:STORE",
