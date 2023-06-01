2b8ab27e1e9a1651c952438115ca683af91a229e
jon <jondwillis@gmail.com>
2023-06-01 16:47:41 -0700

    more layout improvements for GoalSettings
    # Conflicts:
    #	apps/nextjs/src/features/GoalMenu/components/GoalSettings.tsx

--- apps/nextjs/src/features/GoalMenu/components/GoalSettings.tsx
+++ apps/nextjs/src/features/GoalMenu/components/GoalSettings.tsx
@@ -15,12 +15,14 @@ function AdvancedSettingsToggle({ children }) {
   };
 
   return (
-    <Stack direction={{ xs: "column", md: "row" }} gap="1rem">
-      <Link className="m-0 mt-0 p-0" onClick={toggleSettings}>
-        <Typography level="body4" className="m-0 p-0">
-          Advanced Settings {isOpen ? "▲" : "▼"}
-        </Typography>
-      </Link>
+    <Stack direction={{ xs: "column" }} gap="1rem">
+      <Box>
+        <Link className="m-0 mt-0 p-0" onClick={toggleSettings}>
+          <Typography level="body4" color="primary" className="m-0 p-0">
+            Advanced Settings {isOpen ? "▲" : "▼"}
+          </Typography>
+        </Link>
+      </Box>
       {isOpen && <Box>{children}</Box>}
     </Stack>
   );
@@ -37,88 +39,86 @@ function GoalSettings() {
   };
 
   return (
-    <Card variant="soft" color="neutral">
-      <Stack
-        direction={{ xs: "column", md: "column" }}
-        gap="1rem"
-        className="flex"
-      >
-        <Stack direction="row" gap="1rem">
-          <Box>
-            <FormLabel>
-              <Typography level="body2">Auto start</Typography>
-            </FormLabel>
-            <FormHelperText sx={{ mt: 0 }}>
-              <Typography
-                level="body4"
-                className="h-8 w-48 items-center justify-center"
-              >
-                {autoStart
-                  ? 'Planning begins immediately after pressing "Next"'
-                  : "Connect data and tools before starting"}
-              </Typography>
-            </FormHelperText>
-          </Box>
-          <Switch
-            checked={autoStart}
-            onChange={handleSwitchChange}
-            variant="soft"
-            slotProps={{
-              endDecorator: {
-                sx: {
-                  minWidth: 24,
-                },
+    <Stack
+      direction={{ xs: "column", md: "column" }}
+      gap="0rem"
+      className="m-0 flex flex-grow"
+    >
+      <Stack direction="row" gap="0.25rem">
+        <Box>
+          <FormLabel>
+            <Typography level="body2">Auto start</Typography>
+          </FormLabel>
+          <FormHelperText sx={{ mt: 0 }}>
+            <Typography
+              level="body4"
+              className="h-8 w-48 items-center justify-center"
+            >
+              {autoStart
+                ? 'Planning begins immediately after pressing "Next"'
+                : "Connect data and tools before starting"}
+            </Typography>
+          </FormHelperText>
+        </Box>
+        <Switch
+          checked={autoStart}
+          onChange={handleSwitchChange}
+          variant="soft"
+          slotProps={{
+            endDecorator: {
+              sx: {
+                minWidth: 24,
               },
-            }}
-          />
-        </Stack>
-        <AdvancedSettingsToggle>
-          <Stack direction={{ xs: "column", md: "row" }} gap="0.5rem">
-            <Box>
-              <Typography level="body3">Execution Method</Typography>
-              <Select
-                value={executionMethod}
-                onChange={(_event, value) => {
-                  value && setExecutionMethod(value);
-                }}
-              >
-                <Option value="ConversationalReAct">
-                  Fast - Conversational ReAct
-                </Option>
-                <Option value="CoT">Slow - PlanAndExecuteAgentExecutor</Option>
-              </Select>
-            </Box>
-            <Box>
-              <Typography level="body3">Temperature</Typography>
-              <Select
-                value={temperatureOption}
-                onChange={(_, value) => {
-                  value && setTemperatureOption(value);
-                }}
-              >
-                <Option value="Stable">Stable</Option>
-                <Option value="Balanced">Balanced</Option>
-                <Option value="Creative">Creative</Option>
-              </Select>
-            </Box>
-            <Box>
-              <Typography level="body3">Model</Typography>
-              <Select
-                value={llmOption}
-                onChange={(_, value) => {
-                  value && setLLMOption(value);
-                }}
-              >
-                <Option value="gpt-3.5-turbo">gpt-3.5-turbo</Option>
-                <Option value="gpt-3.5-turbo-0314">gpt-3.5-turbo-0314</Option>
-                <Option value="gpt-4">gpt-4</Option>
-                <Option value="gpt-4-0314">gpt-4-0314</Option>
-              </Select>
-            </Box>
-          </Stack>
-        </AdvancedSettingsToggle>
+            },
+          }}
+        />
       </Stack>
-    </Card>
+      <AdvancedSettingsToggle>
+        <Stack direction={{ xs: "column", md: "row" }} gap="0.5rem">
+          <Box>
+            <Typography level="body3">Execution Method</Typography>
+            <Select
+              value={executionMethod}
+              onChange={(_event, value) => {
+                value && setExecutionMethod(value);
+              }}
+            >
+              <Option value="ConversationalReAct">
+                Fast - Conversational ReAct
+              </Option>
+              <Option value="CoT">Slow - PlanAndExecuteAgentExecutor</Option>
+            </Select>
+          </Box>
+          <Box>
+            <Typography level="body3">Temperature</Typography>
+            <Select
+              value={temperatureOption}
+              onChange={(_, value) => {
+                value && setTemperatureOption(value);
+              }}
+            >
+              <Option value="Stable">Stable</Option>
+              <Option value="Balanced">Balanced</Option>
+              <Option value="Creative">Creative</Option>
+            </Select>
+          </Box>
+          <Box>
+            <Typography level="body3">Model</Typography>
+            <Select
+              value={llmOption}
+              onChange={(_, value) => {
+                value && setLLMOption(value);
+              }}
+            >
+              <Option value="gpt-3.5-turbo">gpt-3.5-turbo</Option>
+              <Option value="gpt-3.5-turbo-0314">gpt-3.5-turbo-0314</Option>
+              <Option value="gpt-4">gpt-4</Option>
+              <Option value="gpt-4-0314">gpt-4-0314</Option>
+            </Select>
+          </Box>
+        </Stack>
+      </AdvancedSettingsToggle>
+    </Stack>
   );
 }
 
