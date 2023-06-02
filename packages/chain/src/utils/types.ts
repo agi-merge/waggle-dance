9c4f7680f3f4b0c21d2ffc064969d5e5d3786bd9
jon <jondwillis@gmail.com>
2023-06-01 20:01:43 -0700

    fix 3.5-turbo to be 0314

--- packages/chain/src/utils/types.ts
+++ packages/chain/src/utils/types.ts
@@ -5,7 +5,7 @@ import { type BaseLLMParams } from "langchain/llms/base";
 import { type OpenAIInput } from "langchain/llms/openai";
 
 const TEXT_EMBEDDING_ADA = "text-embedding-ada-002";
-const GPT_35_TURBO = "gpt-3.5-turbo";
+const GPT_35_TURBO = "gpt-3.5-turbo-0314";
 const GPT_4 = "gpt-4-0314";
 // const _GPT_4_32k = "gpt-4-32k";
 export enum LLM {
