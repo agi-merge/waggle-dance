117f3e8d4a032e8770ec73fa9c6963f1bd0aec69
jon <jondwillis@gmail.com>
2023-06-21 19:52:16 -0700

    working on using readResponseStream for plan

--- apps/nextjs/src/features/WaggleDance/utils/readResponseStream.ts
+++ apps/nextjs/src/features/WaggleDance/utils/readResponseStream.ts
@@ -1,17 +1,37 @@
-// utils/streamHandler.ts
-
-export async function readResponseStream(stream: ReadableStream<Uint8Array>, abortSignal: AbortSignal): Promise<Buffer | undefined> {
+export async function readResponseStream(
+    stream: ReadableStream<Uint8Array>,
+    abortSignal: AbortSignal,
+    onDataReceived?: (buffer: Buffer) => void,
+): Promise<Buffer> {
     const reader = stream.getReader();
     let buffer = Buffer.alloc(0);
+    let partialBuffer = Buffer.alloc(0);
+    const processAndReportData = (data: Buffer) => {
+        buffer = Buffer.concat([buffer, data]);
+        if (onDataReceived) {
+            onDataReceived(buffer);
+        }
+    };
 
     try {
         while (!abortSignal.aborted) {
             const { done, value } = await reader.read();
             if (value && value.length > 0) {
-                buffer = Buffer.concat([buffer, Buffer.from(value)]);
+                const newData = Buffer.from(value);
+                const lineBreakIndex = newData.lastIndexOf("\n");
+
+                if (lineBreakIndex !== -1) {
+                    const completeLine = newData.subarray(0, lineBreakIndex + 1);
+                    const partialLine = newData.subarray(lineBreakIndex + 1);
+
+                    processAndReportData(completeLine);
+                    partialBuffer = Buffer.concat([partialBuffer, partialLine]);
+                } else {
+                    processAndReportData(newData);
+                }
             }
             if (done) {
-                return buffer;
+                break;
             }
         }
     } catch (error) {
@@ -21,5 +41,9 @@ export async function readResponseStream(stream: ReadableStream<Uint8Array>, abo
         reader.releaseLock();
     }
 
-    return undefined;
+    if (abortSignal.aborted) {
+        throw new Error("Stream aborted by user");
+    }
+
+    return buffer;
 }
