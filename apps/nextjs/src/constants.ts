047cfa4f79364eaae7310f7aae72697b7ae3560f
jon <jondwillis@gmail.com>
2023-08-08 19:50:45 -0700

    v bump

--- apps/nextjs/src/constants.ts
+++ apps/nextjs/src/constants.ts
@@ -3,7 +3,7 @@
 export const app = {
   name: "waggledance.ai",
   description: "Automate complex tasks with swarms of LLMs",
-  version: process.env.NEXT_PUBLIC_APP_VERSION ?? "05.24.23-alpha",
+  version: process.env.NEXT_PUBLIC_APP_VERSION ?? "alpha 08.08.23",
   localStorageKeys: {
     goal: "AGI:MERGE:GOAL:STORE",
     preferences: "AGI:MERGE:PREFERENCES:STORE",
