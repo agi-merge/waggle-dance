fa809b9d1de7b054f3fe8dd690ead02598d3f9ba
jon <jondwillis@gmail.com>
2023-08-10 13:07:33 -0700

    avoid empty VERCEL_URL since that is what .env.development.local if vercel pull'd has in it

--- apps/nextjs/src/utils/api.ts
+++ apps/nextjs/src/utils/api.ts
@@ -6,7 +6,8 @@ import type { AppRouter } from "@acme/api";
 
 export const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.VERCEL_URL?.length)
+    return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://localhost:3000`; // dev SSR should use localhost
 };
