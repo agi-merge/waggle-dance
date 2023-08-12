349e63a2ce1ad4463fc5f04eea418d8ab62aaab8
jon <jondwillis@gmail.com>
2023-08-11 19:37:03 -0700

    invalidate trpc-managed objects any time a new query succeeds

--- apps/nextjs/src/utils/api.ts
+++ apps/nextjs/src/utils/api.ts
@@ -44,6 +44,24 @@ export const api = createTRPCNext<AppRouter>({
     };
   },
   ssr: true, // trpc automatic SSR also see applicable note at https://trpc.io/docs/client/react/suspense
+  overrides: {
+    useMutation: {
+      /**
+       * This function is called whenever a `.useMutation` succeeds
+       **/
+      async onSuccess(opts) {
+        /**
+         * @note that order here matters:
+         * The order here allows route changes in `onSuccess` without
+         * having a flash of content change whilst redirecting.
+         **/
+        // Calls the `onSuccess` defined in the `useQuery()`-options:
+        await opts.originalFn();
+        // Invalidate all queries in the react-query cache:
+        await opts.queryClient.invalidateQueries();
+      },
+    },
+  },
 });
 
 export { type RouterInputs, type RouterOutputs } from "@acme/api";
