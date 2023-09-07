3335d69c003b7ed4bd9c13b1be60b8e5b2f19d26
jon <jondwillis@gmail.com>
2023-09-06 20:54:28 -0700

    stub schemas for skills

--- packages/db/skills.ts
+++ packages/db/skills.ts
@@ -1,6 +1,10 @@
+import { z } from "zod";
+
 import { type Skillset } from ".";
 
-export type SkillsetCreate = Omit<Skillset, "index" | "id">;
+export type SkillsetCreate = Omit<Skillset, "index" | "id"> & {
+  schema: z.ZodObject<z.ZodRawShape>;
+};
 export type NullableSkillset = Skillset | undefined | null;
 
 const skillsData: SkillsetCreate[] = [
@@ -9,47 +13,55 @@ const skillsData: SkillsetCreate[] = [
     isRecommended: true,
     description:
       "Receive notifications when the AI needs clarification or help with something like account access, or an error has occurred.",
+    schema: z.object({}),
   },
   {
     label: "🏃‍♀️ Context Baton",
     isRecommended: true,
     description: "Parent → Child context passing",
+    schema: z.object({}),
     // risk: "low",
   },
   {
     label: "💭 Memory",
     isRecommended: true,
     description: "Entities, agent internal scratch pad, vector database",
+    schema: z.object({}),
     // risk: "low",
   },
   {
     label: "🗄️ Database",
     isRecommended: true,
     description: "Query databases",
+    schema: z.object({}),
     // risk: "medium",
   },
   {
     label: "✉️ Email",
     isRecommended: false,
     description: "Send emails, react to emails",
+    schema: z.object({}),
     // risk: "high",
   },
   {
     label: "🪝 Webhook",
     isRecommended: false,
     description: "React to webhooks",
+    schema: z.object({}),
     // risk: "low",
   },
   {
     label: "🖥️ REST API",
     isRecommended: false,
     description: "Make HTTP requests",
+    schema: z.object({}),
     // risk: "high",
   },
   {
     label: "🍴 Git",
     isRecommended: false,
     description: "Push, pull, open pull requests, manage CI, etc.",
+    schema: z.object({}),
     // risk: "high",
   },
   {
@@ -57,11 +69,13 @@ const skillsData: SkillsetCreate[] = [
     isRecommended: true,
     description:
       "Use a headless browser to view, scrape, or interact with websites",
+    schema: z.object({}),
     // risk: "medium",
   },
   {
     label: "Files",
     description: "Read, write, and watch files",
+    schema: z.object({}),
     isRecommended: false,
     // risk: "low",
   },
