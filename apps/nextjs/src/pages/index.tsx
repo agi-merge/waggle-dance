56e3baede64df9df0a8e74b2f6352ad60e3fd21f
jon <jondwillis@gmail.com>
2023-09-06 20:14:05 -0700

    redirect instead of import on home :\

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,21 +1,21 @@
 // pages/index.tsx
 
-import { type InferGetServerSidePropsType } from "next";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-import GoalDynamicRoute from "~/pages/goal/[[...goal]]";
-import { getStaticProps as getStaticPropsMain } from "./goal/[[...goal]]";
+// export const getStaticProps = getStaticPropsMain;
 
-export const getStaticProps = getStaticPropsMain;
+// export function getStaticPaths() {
+//   return {
+//     paths: [],
+//     fallback: "blocking",
+//   };
+// }
 
-export function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: "blocking",
-  };
-}
-
-export default function Home({
-  alertConfigs,
-}: InferGetServerSidePropsType<typeof getStaticPropsMain>) {
-  return <GoalDynamicRoute alertConfigs={alertConfigs} />;
+export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    void router.replace("/goal");
+  });
+  return null;
 }
