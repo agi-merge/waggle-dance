b9db28cc7a6a8006385544ebc86ff64eedc75353
jon <jondwillis@gmail.com>
2023-09-09 20:20:38 -0700

    simplify hero subtitle copy

--- apps/nextjs/src/features/GoalPrompt/GoalPrompt.tsx
+++ apps/nextjs/src/features/GoalPrompt/GoalPrompt.tsx
@@ -1,4 +1,4 @@
-import { Link, Tooltip, Typography } from "@mui/joy";
+import { Typography } from "@mui/joy";
 
 import Title from "~/features/MainLayout/components/PageTitle";
 import GoalInput from "./GoalInput";
@@ -8,18 +8,7 @@ export const GoalPrompt = () => {
     <>
       <Title title="🐝 Goal solver">
         <Typography level="body-md">
-          Automate boring, complex tasks with the help of{" "}
-          <Tooltip title="I swear it is a thing" variant="soft" color="neutral">
-            <Link
-              href="https://wikipedia.org/wiki/Waggle_dance"
-              className="font-bold"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              wagglin&apos; swarms{" "}
-            </Link>
-          </Tooltip>
-          of large language model agents
+          Automate complex tasks with a swarm of AI agents.
         </Typography>
       </Title>
       <GoalInput />
