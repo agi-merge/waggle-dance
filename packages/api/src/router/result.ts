b24eff86ab7584b711f85d4e5597438e49a7ea66
jon <jondwillis@gmail.com>
2023-09-11 16:31:26 -0700

    fix create result and updateGraph

--- packages/api/src/router/result.ts
+++ packages/api/src/router/result.ts
@@ -22,52 +22,27 @@ export const resultRouter = createTRPCRouter({
     .mutation(async ({ ctx, input }) => {
       const { goalId, executionId, node, packets, state } = input;
 
-      const nodeOperation = ctx.prisma.executionNode.upsert({
-        where: { id: node.id },
-        create: {
-          id: node.id,
-          name: node.name,
-          context: node.context,
-          graph: { connect: { executionId: executionId } },
-        },
-        update: {
-          name: node.name,
-          context: node.context,
-          graph: { connect: { executionId: executionId } },
+      const result = ctx.prisma.result.create({
+        data: {
+          execution: { connect: { id: executionId } },
+          goal: { connect: { id: goalId } },
+          value: findFinishPacket(packets) as Prisma.InputJsonValue,
+          packets: packets as Prisma.InputJsonValue[],
+          packetVersion: 1,
+          node: {
+            connectOrCreate: {
+              where: { id: node.id },
+              create: node,
+            },
+          },
         },
       });
 
-      const [result, _execution, _graph] = await ctx.prisma.$transaction([
-        ctx.prisma.result.create({
-          data: {
-            execution: { connect: { id: executionId } },
-            goal: { connect: { id: goalId } },
-            value: findFinishPacket(packets) as Prisma.InputJsonValue,
-            packets: packets as Prisma.InputJsonValue[],
-            packetVersion: 1,
-            node: {
-              connect: { id: node.id },
-            },
-          },
-        }),
-        ctx.prisma.execution.update({
-          where: { id: executionId },
-          data: { state },
-        }),
-        ctx.prisma.executionGraph.upsert({
-          where: { executionId: executionId },
-          create: {
-            executionId: executionId,
-            nodes: {
-              connect: [{ id: node.id }],
-            }, // connect to existing nodes
-            edges: { create: [] }, // create empty edges
-          },
-          update: {},
-        }),
-        nodeOperation,
-      ]);
+      const updateExecution = ctx.prisma.execution.update({
+        where: { id: executionId },
+        data: { state },
+      });
 
-      return result;
+      return await ctx.prisma.$transaction([result, updateExecution]);
     }),
 });
