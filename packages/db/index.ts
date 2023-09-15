be00d0d016b38988ea225b393a5d43678489f31b
jon <jondwillis@gmail.com>
2023-09-14 21:48:35 -0700

    fix some edge behavior in transformWireFormat

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -64,38 +64,69 @@ export type OldPlanWireFormat = {
   edges: DraftExecutionEdge[];
 };
 
+/**
+ * Transforms a partial streaming plan from the new format to the old format.
+ *
+ * The new format is a dictionary where each key is a level number and the value is an array of nodes and parents.
+ * Each node is an object with an id, name, and context. The parents are represented as an array of level numbers.
+ *
+ * The old format is an object with two properties: nodes and edges.
+ * Nodes is an array of all nodes, each represented as an object with an id, name, and context.
+ * Edges is an array of all edges, each represented as an object with a sId (source node id) and tId (target node id).
+ *
+ * The function also handles the special case of the criticism node (id 'c'), which depends on all other nodes in the same level.
+ *
+ * @param newFormat - The plan in the new format.
+ * @returns - The plan in the old format.
+ */
 export function transformWireFormat(
   newFormat: PlanWireFormat,
 ): OldPlanWireFormat {
   const oldFormat: OldPlanWireFormat = { nodes: [], edges: [] };
-  const previousLevelNodes: DraftExecutionNode[] = [];
+  const allNodes: { [key: string]: DraftExecutionNode } = {};
+  const criticismSuffix = "c";
 
   for (const level in newFormat) {
     const nodes = newFormat[level];
+    const currentLevelNodes: DraftExecutionNode[] = [];
     for (const item of nodes ?? []) {
       if ("parents" in item && (item.parents as number[])) {
         const parentIds = item.parents as number[];
         for (const parentId of parentIds) {
-          for (const parentNode of previousLevelNodes) {
-            if (parentNode.id === parentId.toString()) {
-              const newNodeId = `${level}-${parentNode.id}`;
-              oldFormat.edges.push({
-                sId: `${level}-${parentId}`,
-                tId: newNodeId,
-              });
-            }
+          const parentNode = allNodes[`${parentId}-${criticismSuffix}`];
+          if (parentNode) {
+            const newNodeId = `${level}-${parentNode.id}`;
+            oldFormat.edges.push({
+              sId: `${parentId}-${parentNode.id}`,
+              tId: newNodeId,
+            });
           }
         }
       } else {
-        const node = item as unknown as DraftExecutionNode; // ok due to logic above
+        const node = item as unknown as DraftExecutionNode;
         const newNodeId = `${level}-${node.id}`;
         oldFormat.nodes.push({
           id: newNodeId,
           name: node.name,
           context: node.context,
         });
-        previousLevelNodes.push(node);
+        currentLevelNodes.push(node);
+        allNodes[newNodeId] = node;
       }
+    }
+
+    const criticismNode = currentLevelNodes.find(
+      (node) => node.id === criticismSuffix,
+    );
+    if (criticismNode) {
+      currentLevelNodes.forEach((node) => {
+        if (node.id !== criticismSuffix) {
+          oldFormat.edges.push({
+            sId: `${level}-${node.id}`,
+            tId: `${level}-${criticismNode.id}`,
+          });
+        }
+      });
     }
   }
 
