c03e63ab42fe3f91e3c97dcc84f474f845d0f1e6
jon <jondwillis@gmail.com>
2023-09-18 22:53:26 -0400

    reset when exe changes

--- apps/nextjs/src/features/WaggleDance/WaggleDance.tsx
+++ apps/nextjs/src/features/WaggleDance/WaggleDance.tsx
@@ -123,6 +123,11 @@ const WaggleDance = ({}: Props) => {
     },
   });
 
+  // when exe changes, be sure to reset!
+  useEffect(() => {
+    reset();
+  }, [execution?.id, reset]);
+
   const handleStart = useCallback(() => {
     if (!isRunning) {
       if (selectedGoal) {
