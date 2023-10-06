2ac6e76cb65de628122fd86f9729293b1148ef90
jon <906671+jondwillis@users.noreply.github.com>
2023-10-06 13:21:48 -0700

    some models cant figure out how to use request human skill and send just a string

--- packages/agent/src/skills/requestUserHelpSkill.ts
+++ packages/agent/src/skills/requestUserHelpSkill.ts
@@ -15,9 +15,19 @@ const requestUserHelpSkill = new DynamicZodSkill({
   name: "requestUserHelp",
   description: `Use only as a last resort before giving up. If you are stuck, or repetitive skill errors occur, you can use this skill to request human help. This will send a message to the USER, who will respond as soon as possible.`,
   func: async (input, _runManager) => {
+    let parsed: { prompt: string };
+
     try {
-      const { prompt: _prompt } = await schema.parseAsync(input);
-    } catch {}
+      // models will sometimes pass strings in, just assume that is the prompt
+      if (typeof input === "string") {
+        parsed = { prompt: input };
+      } else {
+        parsed = await schema.parseAsync(input);
+      }
+    } catch {
+      parsed = { prompt: "error" };
+    }
+    console.debug(`requestUserHelpSkill:`, parsed);
     return `The USER cannot help and is asking you to use your best judgement and to try another route.`;
   },
   schema,
