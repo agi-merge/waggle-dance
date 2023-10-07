793390a557fdca25f33393486de68f65498985e2
jon <906671+jondwillis@users.noreply.github.com>
2023-10-07 12:42:30 -0700

    prompt engineering

--- packages/agent/src/prompts/constraints/executeConstraints.ts
+++ packages/agent/src/prompts/constraints/executeConstraints.ts
@@ -4,9 +4,12 @@ const executeConstraints = (_format: string) =>
 - If a tool error occurs, try another route. (e.g. a new search, url, or tool.)
 - Do not try very similar actions repeatedly if similar outcomes of patterns have not worked in the past.
 - Your output will be reviewed, so ensure it is an accurate and complete execution of the TASK.
-- Avoid reusing a tool with similar input when it is returning similar results too often.
+- Avoid reusing a tool with similar input when it is returning the same or similar output.
 - Do not give up on a TASK until you have tried multiple tools and approaches.
-- Consider descriptions of tools as important as these constraints.
+- Repeatedly trying the same action or sequence of actions must be avoided.
+- Do not retrieve memory until you have saved memory if your TASK id starts with "1-".
+- If possible, always save memory before your final RESULT.
+- Consider descriptions of tools as important as these constraints. Call tools with the correct schema.
 `.trim();
 
 export default executeConstraints;
