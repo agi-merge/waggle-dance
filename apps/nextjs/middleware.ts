c4720fdf3cbe628d052c4c9f16d016b47f25b568
jon <906671+jondwillis@users.noreply.github.com>
2023-10-07 09:52:21 -0700

    try to fix serpapi geo lookups

--- apps/nextjs/middleware.ts
+++ apps/nextjs/middleware.ts
@@ -2,8 +2,15 @@ import { NextResponse, type NextRequest } from "next/server";
 
 import { type Geo } from "@acme/agent";
 
-import countries from "./lib/countries.json";
+import cca2Map from "./lib/cca2Map.json";
 
+type CCA2MapType = {
+  [key: string]: {
+    name: string;
+    divisions: { [key: string]: string };
+    languages: string[];
+  };
+};
 // run only on homepage
 export const config = {
   matcher: "/api/agent/:path*",
@@ -12,19 +19,21 @@ export const config = {
 export function middleware(req: NextRequest) {
   const { nextUrl: url } = req;
   const { geo } = req as { geo: Geo };
-  const country = geo?.country || "United States";
-  // const city = geo?.city || "San Francisco";
-  // const region = geo?.region || "California";
+  const cca2 = geo?.country || "US";
+  const city = geo?.city || "San Francisco";
+  const region = geo?.region || "CA";
 
-  const countryInfo = countries.find((x) => x.cca2 === country);
+  const countryInfo = (cca2Map as CCA2MapType)[cca2];
+  const friendlyCountryName = countryInfo?.name;
+  const friendlyRegionName = countryInfo?.divisions[region];
 
   const languages =
     countryInfo && Object.values(countryInfo.languages).join(", ");
 
-  url.searchParams.set("country", country);
-  // url.searchParams.set("city", city);
-  // url.searchParams.set("region", region);
-  languages && languages[0] && url.searchParams.set("languages", languages[0]);
+  url.searchParams.set("country", friendlyCountryName || cca2);
+  url.searchParams.set("city", city);
+  url.searchParams.set("region", friendlyRegionName || region);
+  languages && url.searchParams.set("languages", languages);
 
   return NextResponse.rewrite(url);
 }
