d4b8b699e4f79534d1e9c27e0e277e064bce129d
jon <906671+jondwillis@users.noreply.github.com>
2023-10-10 11:52:02 -0700

    exponential backoff retry for write lock

--- packages/api/src/router/lock.ts
+++ packages/api/src/router/lock.ts
@@ -1,8 +1,15 @@
 import { createClient } from "@vercel/kv";
 
+import { sleep } from "../sleep";
+
 type LockFunction<T> = () => Promise<T>;
 
-async function withLock<T>(key: string, fn: LockFunction<T>): Promise<T> {
+async function withLock<T>(
+  key: string,
+  fn: LockFunction<T>,
+  retries: number = 5,
+  delay: number = 500,
+): Promise<T> {
   if (!process.env.KV_REST_API_URL) {
     throw new Error("Missing KV_REST_API_URL");
   }
@@ -14,23 +21,31 @@ async function withLock<T>(key: string, fn: LockFunction<T>): Promise<T> {
     url: process.env.KV_REST_API_URL,
     token: process.env.KV_REST_API_TOKEN,
   });
-  // Try to acquire the lock
-  const lockAcquired = await kv.set(`${key}-lock`, "locked", {
-    nx: true,
-    ex: 10,
-  });
 
-  if (!lockAcquired) {
-    throw new Error("Could not acquire lock");
+  let attempt = 0;
+  while (attempt < retries) {
+    // Try to acquire the lock
+    const lockAcquired = await kv.set(`${key}-lock`, "locked", {
+      nx: true,
+      ex: 10,
+    });
+
+    if (lockAcquired) {
+      try {
+        // Execute the function
+        return await fn();
+      } finally {
+        // Release the lock
+        await kv.del(`${key}-lock`);
+      }
+    }
+
+    // If lock not acquired, wait for a bit before retrying
+    await sleep(delay * Math.pow(2, attempt));
+    attempt++;
   }
 
-  try {
-    // Execute the function
-    return await fn();
-  } finally {
-    // Release the lock
-    await kv.del(`${key}-lock`);
-  }
+  throw new Error("Could not acquire lock after multiple attempts");
 }
 
 export default withLock;
