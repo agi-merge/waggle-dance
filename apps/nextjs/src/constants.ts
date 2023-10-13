682c9f6f5e783b0fc9e7fd68f27d606ebe559562
jon <906671+jondwillis@users.noreply.github.com>
2023-10-13 11:50:11 -0700

    default version label to "preview"

--- apps/nextjs/src/constants.ts
+++ apps/nextjs/src/constants.ts
@@ -3,7 +3,7 @@
 export const app = {
   name: "waggledance.ai",
   description: "Automate complex tasks with swarms of LLMs",
-  version: process.env.NEXT_PUBLIC_APP_VERSION ?? "alpha 08.08.23",
+  version: process.env.NEXT_PUBLIC_APP_VERSION ?? "preview",
   localStorageKeys: {
     goal: "AGI:MERGE:GOAL:STORE",
     alerts: "AGI:MERGE:ALERTS:STORE",
