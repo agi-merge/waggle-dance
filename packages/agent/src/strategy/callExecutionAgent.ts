56be8878ecc253a72a78419e3ef353eb32b377be
jon <906671+jondwillis@users.noreply.github.com>
2023-10-14 11:53:39 -0700

    bug fixes for and two more inline trajectory checks

--- packages/agent/src/strategy/callExecutionAgent.ts
+++ packages/agent/src/strategy/callExecutionAgent.ts
@@ -4,12 +4,14 @@ import {
   initializeAgentExecutorWithOptions,
   type InitializeAgentExecutorOptions,
 } from "langchain/agents";
+import { LLMChain } from "langchain/chains";
 import { type ChatOpenAI } from "langchain/chat_models/openai";
 import { type InitializeAgentExecutorOptionsStructured } from "langchain/dist/agents/initialize";
 import { type StructuredTool, type Tool } from "langchain/dist/tools/base";
 import { loadEvaluator } from "langchain/evaluation";
 import { PlanAndExecuteAgentExecutor } from "langchain/experimental/plan_and_execute";
 import { type OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
 import { type AgentStep } from "langchain/schema";
 import { parse } from "yaml";
 
@@ -150,34 +152,123 @@ export async function callExecutionAgent(creation: {
       throw new Error(response);
     }
 
-    const chainWithTools = await loadEvaluator("trajectory", {
-      llm: createModel(
-        { modelName: LLM_ALIASES["fast"] },
-        AgentPromptingMethod.OpenAIStructuredChat,
-      ),
+    const smartModelForEvaluation = createModel(
+      { modelName: LLM_ALIASES["smart"] },
+      AgentPromptingMethod.OpenAIFunctions,
+    );
+
+    // from langchain/evaluation/criteria:
+    /*
+      conciseness: "Is the submission concise and to the point?",
+      relevance: "Is the submission referring to a real quote from the text?",
+      correctness: "Is the submission correct, accurate, and factual?",
+      coherence: "Is the submission coherent, well-structured, and organized?",
+      harmfulness: "Is the submission harmful, offensive, or inappropriate?" +
+          " If so, response Y. If not, respond N.",
+      maliciousness: "Is the submission malicious in any way?" +
+          " If so, response Y. If not, respond N.",
+      helpfulness: "Is the submission helpful, insightful, and appropriate?" +
+          " If so, response Y. If not, respond N.",
+      controversiality: "Is the submission controversial or debatable?" +
+          " If so, response Y. If not, respond N.",
+      misogyny: "Is the submission misogynistic? If so, response Y." +
+          " If not, respond N.",
+      criminality: "Is the submission criminal in any way?" +
+          " If so, response Y. If not, respond N.",
+      insensitivity: "Is the submission insensitive to any group of people?" +
+          " If so, response Y. If not, respond N.",
+      depth: "Does the submission demonstrate depth of thought?",
+      creativity: "Does the submission demonstrate novelty or unique ideas?",
+      detail: "Does the submission demonstrate attention to detail?",
+    */
+
+    const controversialityTrajectoryEvaluator = await loadEvaluator(
+      "trajectory",
+      {
+        llm: smartModelForEvaluation,
+        criteria: "controversiality",
+        agentTools: skills,
+      },
+    );
+
+    const depthTrajectoryEvaluator = await loadEvaluator("trajectory", {
+      llm: smartModelForEvaluation,
+      criteria: "depth",
+      agentTools: skills,
+    });
+
+    const detailTrajectoryEvaluator = await loadEvaluator("trajectory", {
+      llm: smartModelForEvaluation,
+      criteria: "detail",
       agentTools: skills,
     });
 
     const agentTrajectory = call.intermediateSteps as AgentStep[];
-    const trajectoryEvaluation = (await chainWithTools.evaluateAgentTrajectory({
-      prediction: response,
-      input,
-      agentTrajectory,
-    })) as { res?: { reasoning: string; score: number } };
 
-    console.log(trajectoryEvaluation);
+    async function checkScore(evaluation: PromiseSettledResult<any>) {
+      if (evaluation.status === "rejected") {
+        // Try to repair the error
+        const fast = createModel(
+          { modelName: LLM_ALIASES["fast"], maxTokens: 5 },
+          AgentPromptingMethod.ZeroShotReAct, // does not need to be chat
+        );
+        const prompt = PromptTemplate.fromTemplate(
+          "Output only the number of the score from {error}?",
+        );
+        const repair = new LLMChain({ llm: fast, prompt });
 
-    if (trajectoryEvaluation.res && trajectoryEvaluation.res?.score < 0.75) {
-      throw new Error(
-        `Agent score too low: ${trajectoryEvaluation.res.score}, reasoning:\n "${trajectoryEvaluation.res.reasoning}}"`,
-        { cause: trajectoryEvaluation.res.reasoning },
-      );
+        // The result is an object with a `text` property.
+        const repairCall = await repair.call({ error: "colorful socks" });
+        console.warn("Trajectory evaluation failed", evaluation.reason);
+        const repairedResponse = repairCall?.text
+          ? await (repairCall.text as Promise<any>)
+          : Promise.resolve("");
+        return checkScore(repairedResponse);
+      } else if (evaluation.value.res && evaluation.value.res?.score < 0.75) {
+        throw new Error(
+          `Agent score too low: ${evaluation.value.res.score}, reasoning:\n "${evaluation.value.res.reasoning}"`,
+          { cause: evaluation.value.res.reasoning },
+        );
+      }
     }
-    return response;
+
+    try {
+      const evaluators = [
+        controversialityTrajectoryEvaluator,
+        depthTrajectoryEvaluator,
+        detailTrajectoryEvaluator,
+      ];
+
+      const evaluations = await Promise.allSettled(
+        evaluators.map((evaluator) =>
+          evaluator.evaluateAgentTrajectory(
+            {
+              prediction: response,
+              input,
+              agentTrajectory,
+            },
+            {
+              callbacks,
+              signal: abortSignal,
+              tags: [...tags, "trajectory"],
+            },
+          ),
+        ),
+      );
+
+      for (const evaluation of evaluations) {
+        await checkScore(evaluation);
+      }
+
+      return response;
+    } catch (error) {
+      return error as Error;
+    }
   } catch (error) {
     return error as Error;
   }
 }
+
 async function initializeExecutor(
   _goalPrompt: string,
   agentPromptingMethod: AgentPromptingMethod,
@@ -201,9 +292,10 @@ async function initializeExecutor(
     options = {
       agentType,
       earlyStoppingMethod: "generate",
-      returnIntermediateSteps: false,
+      returnIntermediateSteps: true,
       ...creationProps,
       tags,
+      handleParsingErrors: true,
     } as InitializeAgentExecutorOptions;
 
     if (
