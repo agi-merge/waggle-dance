bb7999a49f60a02909bef05897d948c573fc6105
jon <906671+jondwillis@users.noreply.github.com>
2023-10-16 19:44:32 -0700

    being nicer to ai: prompt engineering plan by increasing consistency and more proper prose and being nice

--- packages/agent/src/prompts/createPlanPrompt.ts
+++ packages/agent/src/prompts/createPlanPrompt.ts
@@ -12,6 +12,12 @@ import { stringify as yamlStringify } from "yaml";
 
 import { criticismSuffix } from "./types";
 
+enum PolicyViolation {
+  IGNORES_CONTEXT = "IGNORES_CONTEXT",
+  INCORRECT_FINAL_NODE_NAME = "INCORRECT_FINAL_NODE_NAME",
+  INCORRECT_DEPENDENCIES = "INCORRECT_DEPENDENCIES",
+}
+
 // FIXME: auto-gen this
 export const schema = (format: string) =>
   `
@@ -22,7 +28,7 @@ export const schema = (format: string) =>
   Node
     id: string // e.g. "0", "1", "c" (the node id)
     name: string // a unique-amongst-nodes emoji plus a short description of the node
-    context: string // pass in parameters such as URLs, names, entities, and enough context to efficiently execute the node. Be verbose but focused. Do not use invalid characters in ${format}.
+    context: string // Verbose expectations when done and parameters required to complete Task. Do not use invalid characters in ${format}.
 It is extremely important to return only valid(⚠) ${format} representation of DAG, with levels as the keys.
 `.trim();
 
@@ -156,15 +162,151 @@ const highQualityExamples = [
   },
 ];
 
+const counterExamples = [
+  {
+    input: "Translate the novel 'War and Peace' from Russian to English.",
+    output: {
+      1: [
+        {
+          id: "0",
+          name: "📚 Obtain Russian version of 'War and Peace'",
+          context: "Obtain the Russian version of the novel 'War and Peace'",
+        },
+      ],
+      2: [
+        {
+          parents: ["0"],
+          id: "1",
+          name: "🔀 Translate each sentence independently",
+          context:
+            "Translate each sentence of the novel independently from Russian to English",
+        },
+      ],
+      3: [
+        {
+          parents: ["1"],
+          id: "2",
+          name: "📖 Compile translated sentences into a book",
+          context:
+            "Compile all the translated sentences back into a book format",
+        },
+      ],
+      4: [
+        {
+          parents: ["2"],
+          id: "3",
+          name: "🍯 Goal Delivery",
+          context: "Deliver the translated version of 'War and Peace'",
+        },
+      ],
+    },
+    policyViolation: PolicyViolation.IGNORES_CONTEXT,
+    reason:
+      "Translating each sentence independently ignores the context provided by surrounding sentences. This can lead to incorrect translations, as the meaning of a sentence can often depend on its context. A better approach would be to translate larger sections of text that capture complete thoughts or ideas.",
+  },
+  {
+    input: "Write a blog post about the history of programming languages.",
+    output: {
+      1: [
+        {
+          id: "0",
+          name: "📚 Research the history of programming languages",
+          context:
+            "Gather information about the history of programming languages",
+        },
+        {
+          id: "1",
+          name: "📝 Write the introduction",
+          context: "Write the introduction of the blog post",
+        },
+      ],
+      2: [
+        {
+          parents: ["0"],
+          id: "2",
+          name: "📝 Write the main content",
+          context:
+            "Write the main content of the blog post based on the research",
+        },
+        {
+          parents: ["1", "2"],
+          id: "3",
+          name: "📝 Write the conclusion",
+          context: "Write the conclusion of the blog post",
+        },
+      ],
+      3: [
+        {
+          parents: ["3"],
+          id: "4",
+          name: "📝 Finalize the blog post",
+          context: "Finalize the blog post and prepare it for publishing",
+        },
+      ],
+    },
+    policyViolation: PolicyViolation.INCORRECT_FINAL_NODE_NAME,
+    reason:
+      "The name of the last node should be '🍯 Goal Delivery' to indicate that the Goal has been satisfactorily completed. In this case, the last node is named '📝 Finalize the blog post', which does not follow the prompt's instructions.",
+  },
+  {
+    input: "Bake a chocolate cake and deliver it to a friend's house.",
+    output: {
+      1: [
+        {
+          id: "0",
+          name: "🏠 Go to friend's house",
+          context: "Go to the friend's house to know the location",
+        },
+        {
+          id: "1",
+          name: "🍫 Buy chocolate",
+          context: "Buy the chocolate needed for the cake",
+        },
+      ],
+      2: [
+        {
+          parents: ["0"],
+          id: "2",
+          name: "🍰 Bake the cake",
+          context: "Bake the chocolate cake",
+        },
+        {
+          parents: ["2"],
+          id: "3",
+          name: "🎁 Deliver the cake",
+          context: "Deliver the cake to the friend's house",
+        },
+      ],
+      3: [
+        {
+          parents: ["3"],
+          id: "4",
+          name: "🍯 Goal Delivery",
+          context: "Confirm that the cake has been delivered",
+        },
+      ],
+    },
+    policyViolation: PolicyViolation.INCORRECT_DEPENDENCIES,
+    reason:
+      "The Task 'Bake the cake' is not logically dependent on 'Go to friend's house'. The correct order should be 'Buy chocolate', 'Bake the cake', 'Go to friend's house', and then 'Deliver the cake'. This example demonstrates a plan where a node is not logically dependent on its parent.",
+  },
+];
+
 const constraints = (format: string) =>
   `
-- If the GOAL is phrased like a question or chat comment that can be confidently satisfied by responding with a single answer, then the only node should be "🍯 Goal Delivery".
+# General
+- If the Goal is phrased like a question or chat comment that can be confidently satisfied by responding with a single answer, then the only node should be "🍯 Goal Delivery".
+- Escape all special characters such as quotation marks, curly braces, colons, etc, according to ${format} rules.
+- The only thing you must output is valid ${format} that represents the DAG as the root object.
+
+# DAG Construction
 - The DAG shall be constructed in a way such that its parallelism is maximized (siblings maximized, levels minimized.)
 - Sibling nodes within each level can be run in parallel since they will not logically depend on one another, except the criticism node.
-- All levels must eventually lead to a "🍯 Goal Delivery" task which, after executing, ensures that the GOAL has been satisfactorily completed.
-- Escape all special characters such as quotation marks, curly braces, colons, etc, according to ${format} rules.
+- All levels must eventually lead to a "🍯 Goal Delivery" Task which, after executing, ensures that the Goal has been satisfactorily completed.
+
+# Node Management
 - For every level in the DAG, include a single node with id "${criticismSuffix}". It will run after all other nodes in the level have been executed.
-- THE ONLY THING YOU MUST OUTPUT IS valid ${format} that represents the DAG as the root object.`.trim();
+`.trim();
 
 export function createPlanPrompt(params: {
   goalPrompt: string;
@@ -173,33 +315,37 @@ export function createPlanPrompt(params: {
   returnType: "JSON" | "YAML";
 }): ChatPromptTemplate {
   const { goalPrompt, tools, returnType } = params;
-  const prettyTools = tools
-    .map((tool) => {
-      return tool.name;
-    })
-    .join(",");
+  const toolNames = tools.map((tool) => tool.name).join(",");
+
+  const highQualityExamplesWithCounterExample = {
+    Examples: highQualityExamples,
+    "Counter-examples:": counterExamples,
+  };
+
   const template = `
-YOU: A general goal-solving AI employed by the User to solve the User's GOAL.
-TEAM TOOLS: [${prettyTools}]
-GOAL: ${goalPrompt}
-NOW: ${new Date().toString()}
-SCHEMA: ${schema(returnType)}
-CONSTRAINTS: ${constraints(returnType)}
-EXAMPLES:
+# You are a general Goal-solving AI employed by the User to solve the User's Goal.
+- Team Tools: [${toolNames}]
+- Goal: ${goalPrompt}
+- Current Time: ${new Date().toString()}
+- Schema: ${schema(returnType)}
+- Constraints: ${constraints(returnType)}
+- Examples:
 
   ${
     returnType === "JSON"
-      ? jsonStringify(highQualityExamples)
-      : yamlStringify(highQualityExamples)
+      ? jsonStringify(highQualityExamplesWithCounterExample)
+      : yamlStringify(highQualityExamplesWithCounterExample)
   }
-]
-TASK: To come up with an efficient and expert plan to solve the User's GOAL, according to SCHEMA:
+
+# Task: To come up with an efficient and expert plan to solve the User's Goal, according to the schema.
+Thank you so much!
+- User ❤️
 `.trimEnd();
 
   const systemMessagePrompt =
     SystemMessagePromptTemplate.fromTemplate(template);
 
-  const humanTemplate = `My GOAL is: ${goalPrompt}`;
+  const humanTemplate = `My Goal is: ${goalPrompt}`;
   const humanMessagePrompt =
     HumanMessagePromptTemplate.fromTemplate(humanTemplate);
 
