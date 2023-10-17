bf5eb05af539ba658db787196221f2511298b4ef
jon <906671+jondwillis@users.noreply.github.com>
2023-10-16 21:19:43 -0700

    working on error snackbar

--- apps/nextjs/src/features/Alerts/ErrorSnackbar.tsx
+++ apps/nextjs/src/features/Alerts/ErrorSnackbar.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from "react";
-import { Link, Tooltip } from "@mui/joy";
+import { Card, Link, Tooltip } from "@mui/joy";
 import Box from "@mui/joy/Box";
 import Button from "@mui/joy/Button";
 import Typography from "@mui/joy/Typography";
@@ -14,85 +14,107 @@ export default function ErrorSnackbar() {
   const open = useMemo(() => !!error, [error]);
 
   return (
-    <Box
-      sx={{
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Toast.Provider swipeDirection="right">
-        <JoySnackbar
-          open={open}
-          variant="soft"
-          color="danger"
-          type="foreground"
-          duration={60000}
-          sx={(theme) => ({
-            borderRadius: "lg",
-            shadowRadius: "xl",
-            backgroundColor: theme.palette.background.backdrop,
-            backdropFilter: "blur(3px)",
-            "@supports not ((-webkit-backdrop-filter: blur) or (backdrop-filter: blur))":
-              {
-                backgroundColor: theme.palette.background.surface, // Add opacity to the background color
-              },
-          })}
-          onOpenChange={(open) => !open && setError(null)}
-          endDecorator={
+    <Toast.Provider swipeDirection="right">
+      <JoySnackbar
+        open={open}
+        variant="plain"
+        color="danger"
+        type="foreground"
+        duration={60000}
+        sx={{
+          borderRadius: "lg",
+          shadowRadius: "xl",
+          p: 0,
+        }}
+        onOpenChange={(open) => !open && setError(null)}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            maxHeight: "20rem",
+            overflowY: "scroll",
+            overflowX: "clip",
+            maxWidth: "sm",
+          }}
+        >
+          <Box component={Card} variant="soft" color="danger">
+            <Typography
+              level="title-lg"
+              sx={(theme) => ({
+                whiteSpace: "nowrap",
+                position: "sticky",
+                top: "-1rem",
+                m: "-1rem",
+                p: "1rem",
+                textOverflow: "ellipsis",
+                zIndex: 10,
+                backgroundColor: theme.palette.background.popup,
+              })}
+              variant="plain"
+            >
+              {error?.name}
+            </Typography>
+            <Box sx={{ pt: 1 }}>
+              <Tooltip
+                open={isTooltipOpen}
+                title={error?.message}
+                enterDelay={Infinity}
+              >
+                <Typography
+                  component={Link}
+                  onClick={() => setIsTooltipOpen((prev) => !prev)}
+                  level="body-sm"
+                  color="danger"
+                  fontFamily="monospace"
+                  sx={{
+                    whiteSpace: "break-word",
+                  }}
+                >
+                  {error?.message}
+                </Typography>
+              </Tooltip>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              alignSelf: "end",
+              position: "sticky",
+              bottom: 0,
+              right: 0,
+            }}
+          >
             <Button
-              color="danger"
+              size="lg"
+              color="neutral"
               component={Toast.Action}
               altText="Dismiss message"
-              variant="outlined"
-              sx={{ ml: 2 }}
+              variant="soft"
             >
               Dismiss
             </Button>
-          }
-        >
-          <Box>
-            <Typography color="danger" level="title-lg">
-              {error?.name}
-            </Typography>
-            <Tooltip
-              open={isTooltipOpen}
-              title={error?.message}
-              enterDelay={Infinity}
-            >
-              <Typography
-                component={Link}
-                onClick={() => setIsTooltipOpen((prev) => !prev)}
-                level="body-sm"
-                color="danger"
-                fontFamily="monospace"
-              >
-                {error?.message.slice(0, 250)}
-              </Typography>
-            </Tooltip>
           </Box>
-        </JoySnackbar>
-        <Box
-          component={Toast.Viewport}
-          sx={{
-            "--viewport-padding": "25px",
-            position: "fixed",
-            bottom: 0,
-            right: 0,
-            display: "flex",
-            flexDirection: "column",
-            padding: "var(--viewport-padding)",
-            gap: "10px",
-            minWidth: "xs",
-            maxWidth: { sm: "sm", md: "60dvw", lg: "40dvw" },
-            margin: 0,
-            listStyle: "none",
-            zIndex: 2147483647,
-            outline: "none",
-          }}
-        />
-      </Toast.Provider>
-    </Box>
+        </Box>
+      </JoySnackbar>
+      <Box
+        component={Toast.Viewport}
+        sx={{
+          "--viewport-padding": "25px",
+          position: "fixed",
+          bottom: 0,
+          right: 0,
+          display: "flex",
+          flexDirection: "column",
+          padding: "var(--viewport-padding)",
+          gap: "10px",
+          minWidth: "xs",
+          maxWidth: { sm: "sm", md: "60dvw", lg: "40dvw" },
+          margin: 0,
+          listStyle: "none",
+          zIndex: 2147483647,
+          outline: "none",
+        }}
+      />
+    </Toast.Provider>
   );
 }
