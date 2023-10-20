05c8ef31fb014f4f8a4f1f80fa15ad4f7549087d
jon <906671+jondwillis@users.noreply.github.com>
2023-10-19 21:30:10 -0700

    add a bunch more SearchApi skills and fix location

--- packages/agent/src/utils/skills.ts
+++ packages/agent/src/utils/skills.ts
@@ -30,15 +30,34 @@ class GoogleSearch extends SearchApi {
   static lc_name(): string {
     return "Google Search";
   }
+  description =
+    "Scrapes real time Google results. Try to avoid SEO spam and treat results with skepticism.";
   constructor(apiKey: string, params: Omit<SearchApiParameters, "engine">) {
     super(apiKey, { ...params, engine: "google" });
     this.name = "Google Search";
+  }
+}
+class GoogleTrends extends SearchApi {
+  static lc_name(): string {
+    return "Google Trends";
+  }
+  description = "Scrapes real time Google Trends results.";
+  constructor(apiKey: string, params: Omit<SearchApiParameters, "engine">) {
+    // FIXME: wrap to configure data_type
+    super(apiKey, {
+      ...params,
+      engine: "google_trends",
+      data_type: "TIMESERIES",
+    });
+    this.name = "Google Trends";
   }
 }
 class GoogleScholar extends SearchApi {
   static lc_name(): string {
     return "Google Scholar";
   }
+  description =
+    "Scrapes real time results. Google Scholar API lets users search for academic content like articles, theses, and books from various sources. It ranks these items based on their text, author, where they're published, and how often they're cited. This helps users find relevant research quickly.";
   constructor(apiKey: string, params: Omit<SearchApiParameters, "engine">) {
     super(apiKey, { ...params, engine: "google_scholar" });
     this.name = "Google Scholar";
@@ -48,22 +67,52 @@ class GoogleNews extends SearchApi {
   static lc_name(): string {
     return "Google News";
   }
+  description =
+    "Scrapes real-time results. Google News API offers an automated news aggregation service. It collects headlines from various sources globally, categorizes similar articles and presents them based on each user's interests. The API provides access to multiple links for each news story, allowing users to choose their topic of interest and select from different publishers' versions of the story. The selection and ranking of articles are performed by algorithms that assess factors such as the frequency and location of online appearance. It also prioritizes attributes like timeliness, relevance, diversity, and location. The system is impartial, enabling access to a wide range of perspectives for any story. Google is continuously improving News by adding new sources and enhancing its technology, extending its reach to more regions worldwide.";
   constructor(apiKey: string, params: Omit<SearchApiParameters, "engine">) {
     super(apiKey, { ...params, engine: "google_news" });
     this.name = "Google News";
   }
 }
-class YoutubeTranscripts extends SearchApi {
+class YouTube extends SearchApi {
   static lc_name(): string {
-    return "Youtube Transcripts";
+    return "YouTube";
   }
+  description =
+    "YouTube Search API scrapes real-time search results. It parses ads, videos, shorts, search suggestions, channels, playlists, and more. It supports infinite scrolling and all the native YouTube filters.";
   constructor(apiKey: string, params: Omit<SearchApiParameters, "engine">) {
-    super(apiKey, { ...params, engine: "youtube_transcripts" });
-    this.name = "Youtube Transcripts";
+    super(apiKey, { ...params, engine: "youtube" });
+    this.name = "YouTube";
+  }
+}
+// FIXME: video_id is required. this must be wrapped i believe
+// class YouTubeTranscripts extends SearchApi {
+//   static lc_name(): string {
+//     return "YouTube Transcripts";
+//   }
+//   description =
+//     "Required: `video_id` param. You must already have found some `video_id` before using this. YouTube Transcripts API allows you to get real-time transcript/subtitles for a given YouTube video.";
+//   constructor(apiKey: string, params: Omit<SearchApiParameters, "engine">) {
+//     super(apiKey, { ...params, engine: "youtube_transcripts" });
+//     this.name = "YouTube Transcripts";
+//   }
+// }
+class AmazonSearch extends SearchApi {
+  static lc_name(): string {
+    return "Amazon Search";
+  }
+  description =
+    "Scrapes real-time Amazon search results. The Amazon Search API lets developers tap into Amazon's huge product database. You can search for items, get sorted results based on relevance or reviews, and pull product details.";
+  constructor(apiKey: string, params: Omit<SearchApiParameters, "engine">) {
+    super(apiKey, { ...params, engine: "amazon_search" });
+    this.name = "Amazon Search";
   }
 }
 
-function getSearchLocation(geo?: Geo): string {
+function getSearchLocation(
+  destination: "SerpAPI" | "SearchApi",
+  geo?: Geo,
+): { location: string; hl: string; gl: string } {
   const cca2 = geo?.country || "US";
   const city = geo?.city || "San Francisco";
   const region = geo?.region || "CA";
@@ -87,20 +136,29 @@ function getSearchLocation(geo?: Geo): string {
     if (serpGeo.city) {
       geos.push(serpGeo.city);
     }
-    if (serpGeo.region) {
-      geos.push(serpGeo.region);
-    }
-    if (serpGeo.country) {
-      geos.push(serpGeo.country);
+    if (destination !== "SearchApi") {
+      if (serpGeo.region) {
+        geos.push(serpGeo.region);
+      }
+      if (serpGeo.country) {
+        geos.push(serpGeo.country);
+      }
     }
   }
+
+  const hl = languages?.split(",")?.[0]?.toLowerCase() || "en";
+  const gl = geo?.country?.toLowerCase() || "us";
+
   const location =
     geos.length > 0 ? geos.join(",") : "Los Angeles,California,United States";
 
   console.debug(
-    `SerpAPI location: ${location}, versus original: ${JSON.stringify(geo)}}`,
+    `SerpAPI location:
+    "${location}"
+    versus original:
+    ${JSON.stringify(geo)}}`,
   );
-  return location;
+  return { location, hl, gl };
 }
 
 // skill === tool
@@ -129,26 +187,31 @@ function createSkills(
   }
 
   if (process.env.SEARCHAPI_API_KEY?.length) {
+    const { location, gl } = getSearchLocation("SearchApi", geo);
     const params: SearchApiParameters = {
-      location: getSearchLocation(geo),
-      hl: "en", // host language
-      gl: "us", // geographic location
+      location,
+      // hl, // host language
+      gl, // geographic location
     };
 
     const searchTools = [
       new GoogleSearch(process.env.SEARCHAPI_API_KEY, params),
       new GoogleNews(process.env.SEARCHAPI_API_KEY, params),
-      new YoutubeTranscripts(process.env.SEARCHAPI_API_KEY, params),
+      new YouTube(process.env.SEARCHAPI_API_KEY, params),
+      // new YouTubeTranscripts(process.env.SEARCHAPI_API_KEY, params),
       new GoogleScholar(process.env.SEARCHAPI_API_KEY, params),
+      new GoogleTrends(process.env.SEARCHAPI_API_KEY, params),
+      new AmazonSearch(process.env.SEARCHAPI_API_KEY, params),
     ];
 
     tools.push(...searchTools);
   } else if (process.env.SERPAPI_API_KEY?.length) {
+    const { location, gl } = getSearchLocation("SerpAPI", geo);
     tools.push(
       new SerpAPI(process.env.SERPAPI_API_KEY, {
-        location: getSearchLocation(geo),
-        hl: "en", // host language
-        gl: "us", // geographic location
+        location,
+        // hl, // host language
+        gl, // geographic location
       }),
     );
   }
