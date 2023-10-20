b98abf4c5d2b93f3e420ea87fd3aba4cc7c2f5c9
jon <906671+jondwillis@users.noreply.github.com>
2023-10-20 10:33:11 -0700

    add new skills, better defaults

--- packages/db/skills.ts
+++ packages/db/skills.ts
@@ -32,7 +32,7 @@ const skillsData: SkillsetCreate[] = [
   },
   {
     label: "🗄️ Database",
-    isRecommended: true,
+    isRecommended: false,
     description: "Query databases",
     schema: z.object({}),
     // risk: "medium",
@@ -84,6 +84,47 @@ const skillsData: SkillsetCreate[] = [
     label: "Wolfram|Alpha",
     isRecommended: true,
     description: "Query Wolfram|Alpha",
+    schema: z.object({}),
+  },
+  {
+    label: "Google Search",
+    isRecommended: true,
+    description:
+      "Scrapes real time Google results. Try to avoid SEO spam and treat results with skepticism.",
+    schema: z.object({}),
+  },
+  {
+    label: "Google Trends",
+    isRecommended: true,
+    description: "Scrapes real time Google Trends results.",
+    schema: z.object({}),
+  },
+  {
+    label: "Google Scholar",
+    isRecommended: true,
+    description:
+      "Scrapes real time results. Google Scholar API lets users search for academic content like articles, theses, and books from various sources. It ranks these items based on their text, author, where they're published, and how often they're cited. This helps users find relevant research quickly.",
+    schema: z.object({}),
+  },
+  {
+    label: "Google News",
+    isRecommended: true,
+    description:
+      "Scrapes real-time results. Google News API offers an automated news aggregation service. It collects headlines from various sources globally, categorizes similar articles and presents them based on each user's interests. The API provides access to multiple links for each news story, allowing users to choose their topic of interest and select from different publishers' versions of the story. The selection and ranking of articles are performed by algorithms that assess factors such as the frequency and location of online appearance. It also prioritizes attributes like timeliness, relevance, diversity, and location. The system is impartial, enabling access to a wide range of perspectives for any story. Google is continuously improving News by adding new sources and enhancing its technology, extending its reach to more regions worldwide.",
+    schema: z.object({}),
+  },
+  {
+    label: "YouTube",
+    isRecommended: true,
+    description:
+      "YouTube Search API scrapes real-time search results. It parses ads, videos, shorts, search suggestions, channels, playlists, and more. It supports infinite scrolling and all the native YouTube filters.",
+    schema: z.object({}),
+  },
+  {
+    label: "Amazon Search",
+    isRecommended: true,
+    description:
+      "Scrapes real-time Amazon search results. The Amazon Search API lets developers tap into Amazon's huge product database. You can search for items, get sorted results based on relevance or reviews, and pull product details.",
     schema: z.object({}),
   },
 ];
