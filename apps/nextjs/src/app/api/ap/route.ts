d35d3b3491ea6d7843546e03590d200909a41c8b
jon <906671+jondwillis@users.noreply.github.com>
2023-10-23 22:58:42 -0700

    Response.json instead of new Response

--- apps/nextjs/src/app/api/ap/route.ts
+++ apps/nextjs/src/app/api/ap/route.ts
@@ -1,9 +1,8 @@
 import { type NextRequest } from "next/server";
-import { stringify } from "superjson";
 
 import AgentProtocolOpenAPISpec from "~/../lib/AgentProtocol/openapi.json";
 
-export const runtime = "edge"; // 'nodejs' is the default
+export const runtime = "edge";
 export const dynamic = "force-static";
 
 export function GET(req: NextRequest) {
@@ -13,10 +12,7 @@ export function GET(req: NextRequest) {
     spec.servers[0].url = req.nextUrl.origin;
   }
 
-  return new Response(stringify(spec), {
+  return Response.json(spec, {
     status: 200,
-    headers: {
-      "content-type": "application/json",
-    },
   });
 }
