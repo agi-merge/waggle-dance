16b0d25fbb6022e6520c583bd97837a52f5e1e34
jon <906671+jondwillis@users.noreply.github.com>
2023-10-27 15:37:45 -0700

    show agent settings again

--- apps/nextjs/src/features/AgentSettings/components/AgentSettingsToggleButton.tsx
+++ apps/nextjs/src/features/AgentSettings/components/AgentSettingsToggleButton.tsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { Box, Divider, Stack, type CardProps } from "@mui/joy";
+import { ClickAwayListener } from "@mui/base";
+import {
+  Box,
+  Divider,
+  Link,
+  Menu,
+  Stack,
+  Typography,
+  type CardProps,
+} from "@mui/joy";
 
 import AddDocuments from "~/features/AddDocuments/AddDocuments";
 import DocsModal from "~/features/AddDocuments/components/DocsModal";
@@ -7,7 +16,16 @@ import SkillsModal from "~/features/Skills/components/SkillsModal";
 import SkillSelect from "~/features/Skills/SkillSelect";
 import AgentSettings from "../AgentSettings";
 
-function AgentSettingsToggler({}: { children: React.ReactNode }) {
+function AgentSettingsToggler({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClose = (e?: MouseEvent | TouchEvent | undefined) => {
+    e?.preventDefault();
+    setAnchorEl(null);
+    setIsOpen(false);
+  };
+
   return (
     <Stack direction="row" gap="0.5rem">
       <Box className="align-start flex flex-shrink">
@@ -19,6 +37,40 @@ function AgentSettingsToggler({}: { children: React.ReactNode }) {
           <SkillSelect />
         </SkillsModal>
       </Box>
+      <Divider orientation="vertical" />
+      <>
+        <Link
+          className="m-0 mt-0 p-0"
+          onClick={(event) => {
+            event.preventDefault();
+            setAnchorEl(event.currentTarget);
+            setIsOpen(!isOpen);
+          }}
+          aria-haspopup="menu"
+          id="agent-settings-button"
+          aria-controls={"agent-settings-menu"}
+          aria-expanded={isOpen}
+        >
+          <Typography level="body-sm" className="m-0 p-0">
+            {isOpen ? "▼" : "▲"} Agent Settings
+          </Typography>
+        </Link>
+        {isOpen && (
+          <ClickAwayListener onClickAway={handleClose}>
+            <Menu
+              aria-labelledby="agent-settings-button"
+              id="agent-settings-menu"
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleClose}
+              placement="top-start"
+              className="w-64"
+            >
+              {children}
+            </Menu>
+          </ClickAwayListener>
+        )}
+      </>
     </Stack>
   );
 }
