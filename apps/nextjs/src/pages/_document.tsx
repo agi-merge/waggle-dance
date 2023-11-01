8b4b3bfa9231d7a6f9374f7c55de54813ad10f36
jon <906671+jondwillis@users.noreply.github.com>
2023-10-31 20:02:28 -0700

    add nonce to theme.js

--- apps/nextjs/src/pages/_document.tsx
+++ apps/nextjs/src/pages/_document.tsx
@@ -1,12 +1,34 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript,
+  type DocumentContext,
+  type DocumentInitialProps,
+} from "next/document";
 import Script from "next/script";
 
-class MyDocument extends Document {
+interface MyDocumentProps extends DocumentInitialProps {
+  nonce?: string;
+}
+
+class MyDocument extends Document<MyDocumentProps> {
+  static async getInitialProps(ctx: DocumentContext): Promise<MyDocumentProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    const nonce = (ctx.req?.headers["x-nonce"] as string) || undefined;
+
+    return { ...initialProps, nonce };
+  }
+
   render() {
     return (
       <Html>
         <Head>
-          <Script src="/theme.js" strategy="beforeInteractive" />
+          <Script
+            src="/theme.js"
+            strategy="beforeInteractive"
+            nonce={this.props.nonce}
+          />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <link rel="manifest" href="/manifest.json" />
