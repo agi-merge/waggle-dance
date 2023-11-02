e3041521ee55cff0f38aba7cd943744397243f74
jon <906671+jondwillis@users.noreply.github.com>
2023-11-02 12:30:32 -0700

    add worker to CSP?

--- apps/nextjs/middleware.ts
+++ apps/nextjs/middleware.ts
@@ -34,6 +34,7 @@ export function middleware(req: NextRequest) {
     const cspHeader = `
       default-src 'self';
       script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+      worker-src 'self';
       style-src 'self' 'nonce-${nonce}';
       img-src 'self' blob: data:;
       font-src 'self';
