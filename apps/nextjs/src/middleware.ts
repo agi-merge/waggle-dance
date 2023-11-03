a79af88e4c0c44d515b3223335623cef168f7fcb
jon <906671+jondwillis@users.noreply.github.com>
2023-11-03 03:14:20 -0700

    allow bearer

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -26,93 +26,38 @@ export function middleware(req: NextRequest) {
 
     return NextResponse.rewrite(url);
   } else {
-    //     const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-    //     const requestHeaders = new Headers(req.headers);
-    //     // allow bypassing CORS for agent protocol requests
-    //     if (env.NODE_ENV === "development" && env.CORS_BYPASS_URL) {
-    //       if (url.pathname.startsWith("/api/ap/")) {
-    //         const rawAuthToken = req.headers.get("Authorization") || "";
-    //         // Check if the Authorization header is in the expected format
-    //         if (rawAuthToken.startsWith("Bearer ")) {
-    //           const authTokenParts = rawAuthToken.split(" ");
-    //           // Check if the split operation resulted in the expected number of parts
-    //           if (authTokenParts.length === 2) {
-    //             const authToken = authTokenParts[1]!;
-    //             // Remove any non-alphanumeric characters from the token
-    //             // const sanitizedAuthToken = authToken.replace(/[^a-zA-Z0-9]/g, "");
-    //             const cookieValueRegex = /^[a-zA-Z0-9\-_.]+$/;
-    //             if (cookieValueRegex.test(authToken)) {
-    //               requestHeaders.append(
-    //                 "Cookie",
-    //                 `next-auth.session-token=${authToken};`,
-    //               );
-    //             }
-    //           }
-    //         }
-    //         // Looser CSP for development
-    //         const devCspHeader = `
-    //         default-src 'self' ${env.CORS_BYPASS_URL};
-    //         script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${env.CORS_BYPASS_URL};
-    //         worker-src 'self' ${env.CORS_BYPASS_URL};
-    //         style-src 'self' 'nonce-${nonce}' ${env.CORS_BYPASS_URL};
-    //         img-src 'self' blob: data: ${env.CORS_BYPASS_URL};
-    //         font-src 'self' ${env.CORS_BYPASS_URL};
-    //         object-src 'none';
-    //         base-uri 'self';
-    //         form-action 'self';
-    //         frame-ancestors 'none';
-    //         block-all-mixed-content;
-    //       `;
-    //         requestHeaders.set("x-nonce", nonce);
-    //         requestHeaders.set(
-    //           "Content-Security-Policy",
-    //           // Replace newline characters and spaces
-    //           devCspHeader.replace(/\s{2,}/g, " ").trim(),
-    //         );
-    //       }
-    //     } else {
-    //       // Create a variable to hold both URLs
-    //       const cspDomains = `${env.NEXTAUTH_URL} ${env.VERCEL_URL ?? ""}`;
-    //       // Use the variable in your CSP
-    //       const cspHeader = `
-    //       default-src 'self' ${env.CORS_BYPASS_URL};
-    //       script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${
-    //         env.CORS_BYPASS_URL
-    //       };
-    //       worker-src 'self' ${env.CORS_BYPASS_URL};
-    //       style-src 'self' 'nonce-${nonce}' ${env.CORS_BYPASS_URL};
-    //       img-src 'self' blob: data: ${env.CORS_BYPASS_URL};
-    //       font-src 'self' ${env.CORS_BYPASS_URL};
-    //       object-src 'none';
-    //       base-uri 'self';
-    //       form-action 'self';
-    //       frame-ancestors 'none';
-    //       block-all-mixed-content;
-    //   ${
-    //     env.NODE_ENV === "production"
-    //       ? `block-all-mixed-content;\nupgrade-insecure-requests;`
-    //       : ""
-    //   }
-    //   connect-src 'self' ${cspDomains};
-    // `;
-    //       requestHeaders.set("x-nonce", nonce);
-    //       requestHeaders.set(
-    //         "Content-Security-Policy",
-    //         // Replace newline characters and spaces
-    //         cspHeader.replace(/\s{2,}/g, " ").trim(),
-    //       );
-    //       requestHeaders.set("x-nonce", nonce);
-    //       requestHeaders.set(
-    //         "Content-Security-Policy",
-    //         // Replace newline characters and spaces
-    //         cspHeader.replace(/\s{2,}/g, " ").trim(),
-    //       );
-    //     }
-    //     return NextResponse.next({
-    //       headers: requestHeaders,
-    //       request: {
-    //         headers: requestHeaders,
-    //       },
-    //     });
+    // const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+    const requestHeaders = new Headers(req.headers);
+    // allow bypassing CORS for agent protocol requests
+    // if (env.NODE_ENV === "development" && env.CORS_BYPASS_URL) {
+    if (url.pathname.startsWith("/api/ap/")) {
+      const rawAuthToken = req.headers.get("Authorization") || "";
+      // Check if the Authorization header is in the expected format
+      if (rawAuthToken.startsWith("Bearer ")) {
+        const authTokenParts = rawAuthToken.split(" ");
+        // Check if the split operation resulted in the expected number of parts
+        if (authTokenParts.length === 2) {
+          const authToken = authTokenParts[1]!;
+          // Remove any non-alphanumeric characters from the token
+          // const sanitizedAuthToken = authToken.replace(/[^a-zA-Z0-9]/g, "");
+          const cookieValueRegex = /^[a-zA-Z0-9\-_.]+$/;
+          if (cookieValueRegex.test(authToken)) {
+            requestHeaders.append(
+              "Cookie",
+              `${(url.protocol = "https"
+                ? "__Secure-"
+                : "")}next-auth.session-token=${authToken};`,
+            );
+          }
+        }
+
+        return NextResponse.next({
+          headers: requestHeaders,
+          request: {
+            headers: requestHeaders,
+          },
+        });
+      }
+    }
   }
 }
