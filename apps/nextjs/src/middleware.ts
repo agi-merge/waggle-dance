db75d6cd1f622a5704b4e3f4b8b1958c86c21149
jon <906671+jondwillis@users.noreply.github.com>
2023-11-03 01:29:27 -0700

    use nextauth url to set csp

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -83,19 +83,20 @@ export function middleware(req: NextRequest) {
       }
     } else {
       const cspHeader = `
-      default-src 'self';
-      script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-      worker-src 'self';
-      style-src 'self' 'nonce-${nonce}';
-      img-src 'self' blob: data:;
-      font-src 'self';
-      object-src 'none';
-      base-uri 'self';
-      form-action 'self';
-      frame-ancestors 'none';
-      block-all-mixed-content;
-      upgrade-insecure-requests;
-    `;
+        default-src 'self';
+        script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${env.NEXTAUTH_URL};
+        worker-src 'self' ${env.NEXTAUTH_URL};
+        style-src 'self' 'nonce-${nonce}' ${env.NEXTAUTH_URL};
+        img-src 'self' blob: data: ${env.NEXTAUTH_URL};
+        font-src 'self' ${env.NEXTAUTH_URL};
+        object-src 'none';
+        base-uri 'self';
+        form-action 'self';
+        frame-ancestors 'none';
+        block-all-mixed-content;
+        upgrade-insecure-requests;
+        connect-src 'self' ${env.NEXTAUTH_URL};
+      `;
       requestHeaders.set("x-nonce", nonce);
       requestHeaders.set(
         "Content-Security-Policy",
