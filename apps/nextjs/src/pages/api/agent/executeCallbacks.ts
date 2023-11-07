509567140dbd5e36dd424d77382c74bff2e19810
jon <906671+jondwillis@users.noreply.github.com>
2023-11-06 19:59:02 -0800

    split callbacks into their own file

---
+++ apps/nextjs/src/pages/api/agent/executeCallbacks.ts
@@ -0,0 +1,410 @@
+// callbackHandlers.ts
+// callbacks.ts
+
+import { type TextEncoder } from "util";
+import { type NextRequest } from "next/server";
+import {
+  BaseCallbackHandler,
+  type CallbackHandlerMethods,
+} from "langchain/callbacks";
+import { type Document } from "langchain/document";
+import { type Serialized } from "langchain/load/serializable";
+import { type LLMResult } from "langchain/schema";
+import { parse, stringify } from "yaml";
+
+// Ephemeral, in-memory vector store for demo purposes
+
+import { type AgentPromptingMethod } from "@acme/agent/src/utils/llms";
+import { type DraftExecutionGraph, type DraftExecutionNode } from "@acme/db";
+
+import {
+  type AgentPacket,
+  type ModelCreationProps,
+  type TaskState,
+} from "../../../../../../packages/agent";
+
+const maxLogSize = 4096;
+
+export type CreateCallbackParams = {
+  controller: ReadableStreamDefaultController;
+  encoder: TextEncoder;
+  creationProps: ModelCreationProps;
+  goalPrompt: string;
+  parsedGoalId: string;
+  agentPromptingMethod: AgentPromptingMethod;
+  task: DraftExecutionNode;
+  dag: DraftExecutionGraph;
+  revieweeTaskResults: TaskState[];
+  contentType: "application/json" | "application/yaml";
+  abortController: AbortController;
+  namespace: string;
+  req: NextRequest;
+  lastToolInputs?: Map<string, string>;
+};
+
+export const createCallbacks = (
+  handlePacket: (
+    packet: AgentPacket,
+    controller: ReadableStreamDefaultController,
+    encoder: TextEncoder,
+    creationProps: ModelCreationProps,
+    goalPrompt: string,
+    parsedGoalId: string,
+    agentPromptingMethod: AgentPromptingMethod,
+    task: DraftExecutionNode,
+    dag: DraftExecutionGraph,
+    revieweeTaskResults: TaskState[],
+    contentType: "application/json" | "application/yaml",
+    abortController: AbortController,
+    namespace: string,
+    req: NextRequest,
+    lastToolInputs?: Map<string, string>,
+  ) => Promise<void>,
+  params: CreateCallbackParams,
+): CallbackHandlerMethods[] => {
+  const {
+    controller,
+    encoder,
+    creationProps,
+    goalPrompt,
+    parsedGoalId,
+    agentPromptingMethod,
+    task,
+    dag,
+    revieweeTaskResults,
+    contentType,
+    abortController,
+    namespace,
+    req,
+  } = params;
+  return [
+    BaseCallbackHandler.fromMethods({
+      handleRetrieverStart(
+        retriever: Serialized,
+        query: string,
+        runId: string,
+        parentRunId?: string,
+        tags?: string[],
+        metadata?: Record<string, unknown>,
+      ): void | Promise<void> {
+        const packet: AgentPacket = {
+          type: "handleRetrieverStart",
+          retriever,
+          query,
+          runId,
+          parentRunId,
+          tags,
+          metadata,
+        };
+        void handlePacket(
+          packet,
+          controller,
+          encoder,
+          creationProps,
+          goalPrompt,
+          parsedGoalId,
+          agentPromptingMethod,
+          task,
+          dag,
+          revieweeTaskResults,
+          contentType,
+          abortController,
+          namespace,
+          req,
+        );
+      },
+      handleRetrieverEnd(
+        documents: Document[],
+        runId: string,
+        parentRunId?: string,
+        tags?: string[],
+      ): void | Promise<void> {
+        const packet: AgentPacket = {
+          type: "handleRetrieverEnd",
+          documents,
+          runId,
+          parentRunId,
+          tags,
+        };
+
+        void handlePacket(
+          packet,
+          controller,
+          encoder,
+          creationProps,
+          goalPrompt,
+          parsedGoalId,
+          agentPromptingMethod,
+          task,
+          dag,
+          revieweeTaskResults,
+          contentType,
+          abortController,
+          namespace,
+          req,
+        );
+      },
+      handleLLMStart(
+        llm: Serialized,
+        prompts: string[],
+        runId: string,
+        parentRunId?: string,
+        _extraParams?: Record<string, unknown>,
+        _tags?: string[],
+        _metadata?: Record<string, unknown>,
+      ): Promise<void> | void {
+        const packet: AgentPacket = {
+          type: "handleLLMStart",
+          runId,
+          parentRunId,
+          llmHash: hashCode(JSON.stringify(llm)),
+          hash: hashCode(JSON.stringify(prompts)),
+        };
+
+        void handlePacket(
+          packet,
+          controller,
+          encoder,
+          creationProps,
+          goalPrompt,
+          parsedGoalId,
+          agentPromptingMethod,
+          task,
+          dag,
+          revieweeTaskResults,
+          contentType,
+          abortController,
+          namespace,
+          req,
+        );
+      },
+      handleLLMEnd(
+        output: LLMResult,
+        runId: string,
+        parentRunId?: string,
+      ): Promise<void> | void {
+        const packet: AgentPacket = {
+          type: "handleLLMEnd",
+          output,
+          runId,
+          parentRunId,
+        };
+
+        void handlePacket(
+          packet,
+          controller,
+          encoder,
+          creationProps,
+          goalPrompt,
+          parsedGoalId,
+          agentPromptingMethod,
+          task,
+          dag,
+          revieweeTaskResults,
+          contentType,
+          abortController,
+          namespace,
+          req,
+        );
+      },
+      handleLLMError(
+        err: unknown,
+        runId: string,
+        parentRunId?: string | undefined,
+      ): void | Promise<void> {
+        const packet: AgentPacket = createErrorPacket(
+          "handleLLMError",
+          err,
+          runId,
+          parentRunId,
+        );
+
+        void handlePacket(
+          packet,
+          controller,
+          encoder,
+          creationProps,
+          goalPrompt,
+          parsedGoalId,
+          agentPromptingMethod,
+          task,
+          dag,
+          revieweeTaskResults,
+          contentType,
+          abortController,
+          namespace,
+          req,
+        );
+        console.error("handleLLMError", String(err).slice(0, maxLogSize));
+      },
+      handleChainError(
+        err: unknown,
+        runId: string,
+        parentRunId?: string | undefined,
+      ): void | Promise<void> {
+        const packet: AgentPacket = createErrorPacket(
+          "handleChainError",
+          err,
+          runId,
+          parentRunId,
+        );
+
+        void handlePacket(
+          packet,
+          controller,
+          encoder,
+          creationProps,
+          goalPrompt,
+          parsedGoalId,
+          agentPromptingMethod,
+          task,
+          dag,
+          revieweeTaskResults,
+          contentType,
+          abortController,
+          namespace,
+          req,
+        ); // can be 'Output parser not set'
+        console.error("handleChainError", String(err).slice(0, maxLogSize));
+      },
+      handleToolStart(
+        tool: Serialized,
+        input: string,
+        runId: string,
+        parentRunId?: string | undefined,
+      ): void | Promise<void> {
+        const packet: AgentPacket = {
+          type: "handleToolStart",
+          tool,
+          input,
+          runId,
+          parentRunId,
+        };
+
+        void handlePacket(
+          packet,
+          controller,
+          encoder,
+          creationProps,
+          goalPrompt,
+          parsedGoalId,
+          agentPromptingMethod,
+          task,
+          dag,
+          revieweeTaskResults,
+          contentType,
+          abortController,
+          namespace,
+          req,
+        );
+      },
+      handleToolEnd(
+        output: string,
+        runId: string,
+        parentRunId?: string | undefined,
+      ): void | Promise<void> {
+        const packet: AgentPacket = {
+          type: "handleToolEnd",
+          output,
+          runId,
+          parentRunId,
+        };
+
+        void handlePacket(
+          packet,
+          controller,
+          encoder,
+          creationProps,
+          goalPrompt,
+          parsedGoalId,
+          agentPromptingMethod,
+          task,
+          dag,
+          revieweeTaskResults,
+          contentType,
+          abortController,
+          namespace,
+          req,
+        );
+      },
+      handleToolError(
+        err: unknown,
+        runId: string,
+        parentRunId?: string | undefined,
+      ): void | Promise<void> {
+        const packet: AgentPacket = createErrorPacket(
+          "handleToolError",
+          err,
+          runId,
+          parentRunId,
+        );
+
+        void handlePacket(
+          packet,
+          controller,
+          encoder,
+          creationProps,
+          goalPrompt,
+          parsedGoalId,
+          agentPromptingMethod,
+          task,
+          dag,
+          revieweeTaskResults,
+          contentType,
+          abortController,
+          namespace,
+          req,
+        );
+        console.error("handleToolError", String(err).slice(0, maxLogSize));
+      },
+    }),
+  ];
+};
+
+/**
+ * Returns a hash code from a string
+ * @param  {String} str The string to hash.
+ * @return {Number}    A 32bit integer
+ * @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ * credit: https://stackoverflow.com/a/8831937/127422
+ */
+function hashCode(str: string) {
+  let hash = 0;
+  for (let i = 0, len = str.length; i < len; i++) {
+    const chr = str.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+function createErrorPacket(
+  type:
+    | "handleAgentError"
+    | "handleChainError"
+    | "handleLLMError"
+    | "handleToolError"
+    | "handleRetrieverError",
+  error: unknown,
+  runId: string,
+  parentRunId?: string,
+) {
+  let err: unknown;
+  if (error instanceof Error) {
+    err = `${error.name}: ${error.message}\n${error.stack}`;
+  } else if (typeof error === "string") {
+    err = error;
+  } else {
+    err = parse(stringify(error, Object.getOwnPropertyNames(err)));
+    if (!err) {
+      err = stringify(error);
+    }
+  }
+  const packet: AgentPacket = {
+    type,
+    err,
+    runId,
+    parentRunId,
+  };
+  return packet;
+}
