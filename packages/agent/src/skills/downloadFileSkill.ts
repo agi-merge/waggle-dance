c867b1f461cd78a2b6f35be3233ea4cb78a369b6
jon <906671+jondwillis@users.noreply.github.com>
2023-11-16 20:16:04 -0800

    no namespace is needed for read/download file

--- packages/agent/src/skills/downloadFileSkill.ts
+++ packages/agent/src/skills/downloadFileSkill.ts
@@ -4,19 +4,19 @@ import DynamicZodSkill from "./DynamicZodSkill";
 
 const schema = z.object({
   url: z.string().url().describe("One or more of the system FILES' URLs."),
-  namespace: z
-    .string()
-    .min(1)
-    .describe(
-      "A verification token. You must pass the system NAMESPACE variable as the namespace.",
-    ),
+  // namespace: z
+  //   .string()
+  //   .min(1)
+  //   .describe(
+  //     "A verification token. You must pass the system NAMESPACE variable as the namespace.",
+  //   ),
 });
 
 const downloadFileSkill = new DynamicZodSkill({
   name: "Read File",
   description: `Read/Download private files from your cloud filesystem, after they have been provided to you, or previously saved by you.`,
   func: async (input, _runManager) => {
-    const { url, namespace } = schema.parse(input);
+    const { url /*namespace*/ } = schema.parse(input);
     console.debug("downloadFileSkill", url);
     // FIXME: sanitize the url to make sure it's not a security risk, e.g. local filesystem access
     const response = await fetch(url, {
