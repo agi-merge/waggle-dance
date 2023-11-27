c72c2d4a011ffbb1b744b6f37e6c7520ad0cbb45
jon <906671+jondwillis@users.noreply.github.com>
2023-11-27 14:50:21 -0800

    linting

--- apps/nextjs/src/app/docs/react-swagger.tsx
+++ apps/nextjs/src/app/docs/react-swagger.tsx
@@ -4,6 +4,7 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 type Props = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   spec: Record<string, any>,
 };
 
